3b052e1a543401ebbbe5d99018d15af1b3f3fd42
John Avery <laughing_man77@hotmail.com>
2015-03-06 11:31:34 +1100

    updated processorMath - now working (untested)

--- includes/processor/class.ProcessorMath.php
+++ includes/processor/class.ProcessorMath.php
@@ -7,8 +7,6 @@
  *
  * @SEE http://stackoverflow.com/questions/5057320/php-function-to-evaluate-string-like-2-1-as-arithmetic-2-1-1
  * @SEE https://github.com/jlawrence11/Classes
- * TODO: This is temporarily abandoned work, and does not work. Fix this.
- * TODO: Refine this class so that all mathematical functions are allowed, as per eqEOS
  */
 
 include_once(Config::$dirIncludes . 'processor/class.Processor.php');
@@ -16,48 +14,69 @@ include_once(Config::$dirIncludes . '/math/eos.class.php');
 
 class ProcessorMath extends Processor
 {
-  protected $required = array('vars', 'operator');
-  private $validOperators = array('<', '>', '==', '===', '+', '-', '/', '*', '%', '^');
+  protected $required = array('vars', 'formula');
+  protected $details = array(
+    'name' => 'Math',
+    'description' => 'Allows a simple or complex equation to be applied to input values. Variable names must start with a letter, and in the formula, the same variable name must be prefixed with "$"',
+    'menu' => 'compute',
+    'input' => array(
+      'vars' => array(
+        'description' => 'The inputs to the equation (variables and static values).',
+        'cardinality' => array(1, '*'),
+        'accepts' => array('processor', 'mixed')
+      ),
+      'var_names' => array(
+        'description' => 'The names of the variables. These map 1:1 with vars.',
+        'cardinality' => array(1, '*'),
+        'accepts' => array('processor', 'mixed')
+      ),
+      'equation' => array(
+        'description' => 'The equation.',
+        'cardinality' => array(1, '*'),
+        'accepts' => array('string')
+      ),
+    ),
+  );
 
   public function process()
   {
-    Debug::variable($this->meta, 'ProcessorCompare');
+    Debug::variable($this->meta, 'ProcessorMath');
     $required = $this->validateRequired();
     if ($required !== TRUE) {
       return $required;
     }
 
-    $operator = $this->getVar($this->meta->operator);
-    if ($this->status != 200) {
-      return $operator;
-    }
-    if (!in_array($operator, $this->validOperators)) {
-      $this->status = 417;
-      return new Error(3, $this->id, "Invalid operator");
+    if (sizeof($this->meta->vars) !== sizeof($this->meta->var_names)) {
+      return new Error(3, $this->id, 'invalid mapping of var names to vars');
     }
 
-    $vars = array();
-    $count = 1;
-    foreach ($this->meta->vars as $var) {
-      $v = $this->getVar($var);
+    $vars = $this->meta->vars;
+    $variables = array();
+    $index = 0;
+    foreach ($vars as $var) {
+      $value = $this->getVar($var);
       if ($this->status != 200) {
-        return $v;
+        return $value;
       }
-      if (!is_string($v) && !is_numeric($v)) {
-        $this->status = 417;
-        Debug::variable($v);
-        return new Error(3, $this->id, "variable $count did not produce a string or number. Could not compute");
+      $name = $this->getVar($this->meta->var_names[$index++]);
+      if ($this->status != 200) {
+        return $name;
       }
-      $vars[] = $v;
+      $variables[$name] = $value;
     }
 
-    return $this->doOperation($vars, $operator);
-  }
+    $equation = $this->getVar($this->meta->equation);
+    if ($this->status != 200) {
+      return $equation;
+    }
 
-  private function doOperation($vars, $operator)
-  {
-    $equation = implode($operator, $vars);
-    $eq = new eqEOS();
-    return $eq->solveIF($equation);
+    try {
+      $eq = new eqEOS();
+      $result = $eq->solveIF($equation, $variables);
+    } catch (Exception $e) {
+      return new Error(3, $this->id, 'could not process equation. ' . $e->getMessage());
+    }
+
+    return $result;
   }
 }
