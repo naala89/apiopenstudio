a4c72798967999c9b5d0bcf3b02cf93ae5590bd9
John Avery <laughing_man77@hotmail.com>
2015-09-09 10:23:06 +1000

    tidy validators and added TokenUser class

---
+++ includes/validator/TokenUser.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Provide token authentication based on token
+ *
+ * Meta:
+ *    {
+ *      "type": "token",
+ *      "meta": {
+ *        "id":<integer>,
+ *        "token": <processor|string>
+ *      }
+ *    }
+ */
+
+namespace Datagator\Validator;
+use Datagator\Core;
+
+class TokenUser extends Token {
+  protected $role = false;
+  protected $required = array('token');
+  protected $details = array(
+    'name' => 'Token User',
+    'description' => 'Validate the request, and only allow for a user.',
+    'menu' => 'validator',
+    'client' => 'All',
+    'application' => 'All',
+    'input' => array(
+      'usernames' => array(
+        'description' => "The username/s.",
+        'cardinality' => array(1, '*'),
+        'accepts' => array('processor', 'literal', 'array'),
+      ),
+    ),
+  );
+
+  /**
+   * @return bool
+   * @throws \Datagator\Core\ApiException
+   */
+  public function process() {
+    Core\Debug::variable($this->meta, 'Validator TokenConsumer', 4);
+    $this->validateRequired();
+
+    // check user exists
+    $this->request->user->findByToken($this->getVar($this->meta->token));
+    if (!$this->request->user->exists() || !$this->request->user->isActive()) {
+      throw new Core\ApiException('permission denied', -1, $this->id, 401);
+    }
+    // check user is in the list of valid users
+    $usernames = $this->getVar($this->meta->usernames);
+    if (!is_array($usernames)) {
+      $usernames = array($usernames);
+    }
+    foreach ($usernames as $username) {
+      if ($username != $this->request->user->getUsername()) {
+        throw new Core\ApiException('permission denied', -1, $this->id, 401);
+      }
+    }
+
+    return TRUE;
+  }
+}
