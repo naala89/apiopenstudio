4ff36b6238143b57c8f4ca19d31ecb130efd8e28
John <3242482-john89@users.noreply.gitlab.com>
2016-04-22 17:02:25 +1000

    updated db craetion scripts

--- includes/scripts/db/data.sql
+++ includes/scripts/db/data.sql
@@ -1,58 +1,109 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.9.1
+-- version 4.0.10.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 21, 2016 at 06:11 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.11
+-- Generation Time: Apr 22, 2016 at 04:59 PM
+-- Server version: 5.5.48-cll
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `datagator`
+-- Database: `datagato_api`
 --
 
+--
+-- Truncate table before insert `account`
+--
+
+TRUNCATE TABLE `account`;
 --
 -- Dumping data for table `account`
 --
 
 INSERT INTO `account` (`accid`, `uid`, `name`) VALUES
-(1, 1, 'Datagator');
+(1, 1, ''Datagator'');
 
+--
+-- Truncate table before insert `application`
+--
+
+TRUNCATE TABLE `application`;
 --
 -- Dumping data for table `application`
 --
 
 INSERT INTO `application` (`appid`, `accid`, `name`) VALUES
-(1, 1, 'System'),
-(2, 1, 'All');
+(1, 1, ''System''),
+(2, 1, ''All'');
 
+--
+-- Truncate table before insert `blacklist`
+--
+
+TRUNCATE TABLE `blacklist`;
+--
+-- Truncate table before insert `external_user`
+--
+
+TRUNCATE TABLE `external_user`;
+--
+-- Truncate table before insert `log`
+--
+
+TRUNCATE TABLE `log`;
+--
+-- Truncate table before insert `resource`
+--
+
+TRUNCATE TABLE `resource`;
 --
 -- Dumping data for table `resource`
 --
 
-INSERT INTO `resource` (`appid`, `method`, `identifier`, `meta`, `ttl`) VALUES
-(1, 'post', 'systemcache', 'A"process":{"processor":"systemCache","meta":{"id":3,"operation":{"processor":"varUri","meta":{"id":4,"index":0}}}}}', 0),
-(2, 'post', 'userlogin', '{"process":{"processor":"UserLogin","meta":{"id":1,"username":{"processor":"VarPost","meta":{"id":2,"name":"username"}},"password":{"processor":"VarPost","meta":{"id":3,"name":"password"}}}}}', 300),
-(2, 'get', 'resourceyaml', '{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}', 300),
-(2, 'post', 'resourceyaml', '{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3}}}', 300),
-(2, 'delete', 'resourceyaml', '{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}', 300),
-(2, 'post', 'userlogindrupal', '{"process":{"processor":"loginStoreDrupal","meta":{"id":1,"source":{"processor":"inputUrl","meta":{"id":2,"source":{"processor":"concatenate","meta":{"id":3,"sources":[{"processor":"varStore","meta":{"id":4,"name":"drupalUrl","operation":"fetch"}},"api\\/anon\\/user\\/login"]}},"method":"post","0":{"username":{"processor":"varPost","meta":{"id":5,"name":"username"}},"password":{"processor":"varPost","meta":{"id":6,"name":"password"}}},"curlOpts":{"CURLOPT_SSL_VERIFYPEER":0,"CURLOPT_FOLLOWLOCATION":1}}}}}}', 0),
-(2, 'get', 'processorsall', '{"process":{"processor":"ProcessorsAll","meta":{"id":3}},"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}}}', 3600),
-(2, 'post', 'resourcejson', '{"process":{"processor":"resourceJson","meta":{"id":3}}}', 300),
-(2, 'get', 'resourcejson', '{"process":{"processor":"resourceJson","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}', 300),
-(2, 'delete', 'resourcejson', '{"process":{"processor":"resourceJson","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}', 300);
+INSERT INTO `resource` (`id`, `appid`, `method`, `identifier`, `meta`, `ttl`) VALUES
+(54, 1, ''post'', ''systemcache'', ''A"process":{"processor":"systemCache","meta":{"id":3,"operation":{"processor":"varUri","meta":{"id":4,"index":0}}}}}'', 0),
+(55, 2, ''post'', ''userlogin'', ''{"process":{"processor":"UserLogin","meta":{"id":1,"username":{"processor":"VarPost","meta":{"id":2,"name":"username"}},"password":{"processor":"VarPost","meta":{"id":3,"name":"password"}}}}}'', 300),
+(56, 2, ''get'', ''resourceyaml'', ''{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}'', 300),
+(57, 2, ''post'', ''resourceyaml'', ''{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3}}}'', 300),
+(58, 2, ''delete'', ''resourceyaml'', ''{"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}},"process":{"processor":"resourceYaml","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}'', 300),
+(59, 2, ''post'', ''userlogindrupal'', ''{"process":{"processor":"loginStoreDrupal","meta":{"id":1,"source":{"processor":"inputUrl","meta":{"id":2,"source":{"processor":"concatenate","meta":{"id":3,"sources":[{"processor":"varStore","meta":{"id":4,"name":"drupalUrl","operation":"fetch"}},"api\\/anon\\/user\\/login"]}},"method":"post","0":{"username":{"processor":"varPost","meta":{"id":5,"name":"username"}},"password":{"processor":"varPost","meta":{"id":6,"name":"password"}}},"curlOpts":{"CURLOPT_SSL_VERIFYPEER":0,"CURLOPT_FOLLOWLOCATION":1}}}}}}'', 0),
+(60, 2, ''get'', ''processorsall'', ''{"process":{"processor":"ProcessorsAll","meta":{"id":3}},"validation":{"processor":"TokenDeveloper","meta":{"id":1,"token":{"processor":"varGet","meta":{"id":2,"name":"token"}}}}}'', 3600),
+(61, 2, ''post'', ''resourcejson'', ''{"process":{"processor":"resourceJson","meta":{"id":3}}}'', 300),
+(62, 2, ''get'', ''resourcejson'', ''{"process":{"processor":"resourceJson","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}'', 300),
+(63, 2, ''delete'', ''resourcejson'', ''{"process":{"processor":"resourceJson","meta":{"id":3,"method":{"processor":"varGet","meta":{"id":4,"name":"method"}},"noun":{"processor":"varGet","meta":{"id":5,"name":"noun"}},"verb":{"processor":"varGet","meta":{"id":6,"name":"verb"}}}}}'', 300);
 
+--
+-- Truncate table before insert `role`
+--
+
+TRUNCATE TABLE `role`;
 --
 -- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`rid`, `name`) VALUES
-(1, 'sys-admin'),
-(2, 'admin'),
-(3, 'developer'),
-(4, 'consumer');
+(1, ''sys-admin''),
+(2, ''admin''),
+(3, ''developer''),
+(4, ''consumer'');
 
+--
+-- Truncate table before insert `user`
+--
 
+TRUNCATE TABLE `user`;
+
+--
+-- Truncate table before insert `user_role`
+--
+
+TRUNCATE TABLE `user_role`;
+
+--
+-- Truncate table before insert `vars`
+--
+
+TRUNCATE TABLE `vars`;
--- includes/scripts/db/structure.sql
+++ includes/scripts/db/structure.sql
@@ -1,17 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.9.1
+-- version 4.0.10.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 21, 2016 at 06:21 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.11
+-- Generation Time: Apr 22, 2016 at 04:58 PM
+-- Server version: 5.5.48-cll
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `datagator`
+-- Database: `datagato_api`
 --
 
 -- --------------------------------------------------------
@@ -22,10 +22,11 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE IF NOT EXISTS `account` (
-  `accid` int(10) unsigned NOT NULL COMMENT 'account id',
+  `accid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'account id',
   `uid` int(10) unsigned NOT NULL COMMENT 'user id',
-  `name` varchar(255) NOT NULL COMMENT 'name of account'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `name` varchar(255) NOT NULL COMMENT 'name of account',
+  PRIMARY KEY (`accid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -35,10 +36,11 @@ CREATE TABLE IF NOT EXISTS `account` (
 
 DROP TABLE IF EXISTS `application`;
 CREATE TABLE IF NOT EXISTS `application` (
-  `appid` int(10) unsigned NOT NULL COMMENT 'application id',
+  `appid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'application id',
   `accid` int(10) unsigned NOT NULL COMMENT 'account id',
-  `name` varchar(255) NOT NULL COMMENT 'application name'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `name` varchar(255) NOT NULL COMMENT 'application name',
+  PRIMARY KEY (`appid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -48,11 +50,12 @@ CREATE TABLE IF NOT EXISTS `application` (
 
 DROP TABLE IF EXISTS `blacklist`;
 CREATE TABLE IF NOT EXISTS `blacklist` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `appid` int(10) unsigned NOT NULL COMMENT 'application id',
   `min_ip` varchar(32) NOT NULL,
-  `max_ip` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `max_ip` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -62,14 +65,15 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
 
 DROP TABLE IF EXISTS `external_user`;
 CREATE TABLE IF NOT EXISTS `external_user` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `appid` int(10) unsigned NOT NULL COMMENT 'appplication id',
   `external_id` varchar(255) DEFAULT NULL COMMENT 'user id in external entity',
   `external_entity` varchar(255) NOT NULL COMMENT 'name of the external entity',
   `data_field_1` varchar(255) DEFAULT NULL,
   `data_field_2` varchar(255) DEFAULT NULL,
-  `data_field_3` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `data_field_3` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -79,13 +83,15 @@ CREATE TABLE IF NOT EXISTS `external_user` (
 
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `appid` int(10) unsigned NOT NULL COMMENT 'the application this log belongs to',
   `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user` varchar(256) DEFAULT NULL,
   `ip` varchar(11) DEFAULT NULL,
   `type` varchar(64) NOT NULL,
-  `text` varchar(1024) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
+  `text` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 -- --------------------------------------------------------
 
@@ -95,13 +101,14 @@ CREATE TABLE IF NOT EXISTS `log` (
 
 DROP TABLE IF EXISTS `resource`;
 CREATE TABLE IF NOT EXISTS `resource` (
-  `id` int(10) unsigned NOT NULL COMMENT 'resource id',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'resource id',
   `appid` int(10) unsigned DEFAULT NULL COMMENT 'client id',
   `method` text NOT NULL COMMENT 'form delivery method',
   `identifier` varchar(64) NOT NULL COMMENT 'identifier of the api call',
   `meta` varchar(16384) NOT NULL COMMENT 'all of the actions taken by the call',
-  `ttl` int(10) unsigned NOT NULL DEFAULT '300' COMMENT 'time to cache the results (seconds)'
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+  `ttl` int(10) unsigned NOT NULL DEFAULT '300' COMMENT 'time to cache the results (seconds)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
 
 -- --------------------------------------------------------
 
@@ -111,9 +118,10 @@ CREATE TABLE IF NOT EXISTS `resource` (
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
-  `rid` int(10) unsigned NOT NULL COMMENT 'role id',
-  `name` varchar(255) NOT NULL COMMENT 'title of role'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `rid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'role id',
+  `name` varchar(255) NOT NULL COMMENT 'title of role',
+  PRIMARY KEY (`rid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -123,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `role` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `uid` int(10) unsigned NOT NULL COMMENT 'user id',
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'user id',
   `active` tinyint(4) NOT NULL DEFAULT '1',
   `username` varchar(256) NOT NULL,
   `salt` varchar(16) DEFAULT NULL COMMENT 'Salt for the password hash',
@@ -142,8 +150,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `address_state` varchar(255) DEFAULT NULL,
   `address_postcode` varchar(8) DEFAULT NULL,
   `phone_mobile` int(11) DEFAULT NULL,
-  `phone_work` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `phone_work` int(11) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -153,11 +162,12 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE IF NOT EXISTS `user_role` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uid` int(10) unsigned NOT NULL COMMENT 'user id',
   `rid` int(10) unsigned NOT NULL COMMENT 'role id',
-  `appid` int(10) unsigned NOT NULL COMMENT 'application id'
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `appid` int(10) unsigned NOT NULL COMMENT 'application id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 -- --------------------------------------------------------
 
@@ -167,127 +177,9 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 
 DROP TABLE IF EXISTS `vars`;
 CREATE TABLE IF NOT EXISTS `vars` (
-  `id` int(11) unsigned NOT NULL COMMENT 'id of the var',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id of the var',
   `appid` int(11) unsigned NOT NULL COMMENT 'client id',
   `name` varchar(256) NOT NULL COMMENT 'name of the var',
-  `val` varchar(256) DEFAULT NULL COMMENT 'value of the var'
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-ADD PRIMARY KEY (`accid`);
-
---
--- Indexes for table `application`
---
-ALTER TABLE `application`
-ADD PRIMARY KEY (`appid`);
-
---
--- Indexes for table `blacklist`
---
-ALTER TABLE `blacklist`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `external_user`
---
-ALTER TABLE `external_user`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `log`
---
-ALTER TABLE `log`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `resource`
---
-ALTER TABLE `resource`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-ADD PRIMARY KEY (`rid`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-ADD PRIMARY KEY (`uid`);
-
---
--- Indexes for table `user_role`
---
-ALTER TABLE `user_role`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `vars`
---
-ALTER TABLE `vars`
-ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-MODIFY `accid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'account id',AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `application`
---
-ALTER TABLE `application`
-MODIFY `appid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'application id',AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `blacklist`
---
-ALTER TABLE `blacklist`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `external_user`
---
-ALTER TABLE `external_user`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `log`
---
-ALTER TABLE `log`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=47;
---
--- AUTO_INCREMENT for table `resource`
---
-ALTER TABLE `resource`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'resource id',AUTO_INCREMENT=44;
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-MODIFY `rid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'role id',AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `uid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'user id',AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `user_role`
---
-ALTER TABLE `user_role`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `vars`
---
-ALTER TABLE `vars`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id of the var',AUTO_INCREMENT=13;
+  `val` varchar(256) DEFAULT NULL COMMENT 'value of the var',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
