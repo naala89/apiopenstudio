3a869c0e33a17342daa945d797c40d824b443757
John Avery <laughing_man77@hotmail.com>
2016-04-23 21:57:31 +1000

    Object working

--- includes/processor/Object.php
+++ includes/processor/Object.php
@@ -19,33 +19,36 @@
 namespace Datagator\Processor;
 use Datagator\Core;
 
-class Object extends ProcessorBase
-{
+class Object extends ProcessorBase {
+
   protected $details = array(
     'name' => 'Object',
-    'description' => 'Create a custom object from inputs. This is useful for creating an output of object from selected input fields. You can use field processor for name value pairs, or other processors or literals to create single values.',
+    'description' => 'Create a custom object from inputs. This is useful for creating an output of object from selected input fields. You can use field processor for name value pairs, or other processors or literals to create single values. It can also be used to parse XML, JSON input from an external source into an object that you can work with.',
     'menu' => 'Primitive',
     'application' => 'All',
     'input' => array(
       'attributes' => array(
-        'description' => 'A single attribute value or a Field.',
+        'description' => 'The value of an attribute or a complex object.',
         'cardinality' => array(0, '*'),
         'accepts' => array('processor', 'literal'),
       ),
     ),
   );
 
-  public function process()
-  {
+  public function process() {
     Core\Debug::variable($this->meta, 'Processor Object', 4);
 
     $result = array();
     $attributes = $this->val($this->meta->attributes);
-    foreach ($attributes as $attribute) {
-      $val = $this->isProcessor($attribute) ? $this->val($attribute) : $attribute;
-      $result[] = $val;
-    }
 
+    if (sizeof($attributes) == 1) {
+      $result = $attributes[0];
+    } else {
+      foreach ($attributes as $attribute) {
+        $result[] = $attribute;
+      }
+    }
+var_dump($result);exit;
     return $result;
   }
 }
