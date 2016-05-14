395a25c76c61d82fa7666df71db2b2bc4a54b2a3
John Avery <laughing_man77@hotmail.com>
2016-05-15 05:35:06 +1000

    finished login tests and helper

--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -6,5 +6,35 @@ namespace Helper;
 
 class Api extends \Codeception\Module
 {
+  private $token='';
 
+  /**
+   * @throws \Codeception\Exception\ModuleException
+   */
+  public function storeMyToken()
+  {
+    $response = $this->getModule('REST')->response;
+    $arr = \GuzzleHttp\json_decode(\GuzzleHttp\json_encode(\GuzzleHttp\json_decode($response)), true);
+    if (isset($arr['token'])) {
+      $this->token = $arr['token'];
+    }
+  }
+
+  /**
+   * @return string
+   */
+  public function getMyStoredToken()
+  {
+    return $this->token;
+  }
+
+  /**
+   * @throws \Codeception\Exception\ModuleException
+   */
+  public function seeTokenIsSameAsStoredToken()
+  {
+    $response = $this->getModule('REST')->response;
+    $arr = \GuzzleHttp\json_decode(\GuzzleHttp\json_encode(\GuzzleHttp\json_decode($response)), true);
+    \PHPUnit_Framework_Assert::assertEquals($this->token, $arr['token']);
+  }
 }
