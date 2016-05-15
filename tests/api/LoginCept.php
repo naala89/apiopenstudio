31c51074eabce1ccaaa0a54831cdaaee84c4bddc
John <3242482-john89@users.noreply.gitlab.com>
2016-05-16 09:34:36 +1000

    consolidated login tests

---
+++ tests/api/LoginCept.php
@@ -0,0 +1,37 @@
+<?php
+
+$I = new ApiTester($scenario);
+$I->wantTo('perform a successful login and see result');
+$I->haveHttpHeader('Accept', 'application/json');
+$I->sendPOST('/4/user/login', ['username' => 'tester', 'password' => 'tester_pass']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseMatchesJsonType(array('token' => 'string'));
+$I->storeMyToken();
+
+$I = new ApiTester($scenario);
+$I->wantTo('perform an invalid login and see result');
+$I->haveHttpHeader('Accept', 'application/json');
+$I->sendPOST('/4/user/login', ['username' => 'tester', 'password' => 'wrong_passs']);
+$I->seeResponseCodeIs(401);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array(
+  'error' => array(
+    'code' => 4,
+    'message' => 'Invalid username or password.',
+    'id' => -1
+  )
+));
+
+$I = new ApiTester($scenario);
+$I->wantTo('validate that token is not recreated before ttl expires');
+$I->haveHttpHeader('Accept', 'application/json');
+$I->sendPOST('/4/user/login', ['username' => 'tester', 'password' => 'tester_pass']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseMatchesJsonType(array('token' => 'string'));
+$I->storeMyToken();
+$I->sendPOST('/4/user/login', ['username' => 'tester', 'password' => 'tester_pass']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeTokenIsSameAsStoredToken();
