54b80b5ed00a870bac6db98e7ef3b780be453eea
John <3242482-john89@users.noreply.gitlab.com>
2016-05-20 15:22:55 +1000

    fix tests for IfThenElse

--- tests/api/IfThenElseCept.php
+++ tests/api/IfThenElseCept.php
@@ -7,103 +7,127 @@ $I->createResourceFromYaml();
 
 $I->wantTo('test IfThenElse == operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '=='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse == operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '=='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse == operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '=='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse == operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '=='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse == operator when == but not strict and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => '100', 'operator' => '=='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse != operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '!='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse != operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '!='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse != operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '!='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse != operator when == but not strict and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => '100', 'operator' => '!='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse < operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse < operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse < operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse < operator with letters when < and see result');
 $I->callResourceFromYaml(array('lhs' => 'a', 'rhs' => 'b', 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse < operator with letters when > and see result');
 $I->callResourceFromYaml(array('lhs' => 'b', 'rhs' => 'a', 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse < operator with letters when == and see result');
 $I->callResourceFromYaml(array('lhs' => 'a', 'rhs' => 'a', 'operator' => '<'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse > operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '>'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse > operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '>'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse > operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '>'));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse >= operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '>='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse >= operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '>='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse >= operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '>='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse <= operator when < and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 200, 'operator' => '<='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse <= operator when > and see result');
 $I->callResourceFromYaml(array('lhs' => 200, 'rhs' => 100, 'operator' => '<='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('false');
 
 $I->wantTo('test IfThenElse <= operator when == and see result');
 $I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '<='));
+$I->seeResponseCodeIs(200);
 $I->seeResponseContains('true');
 
 $I->wantTo('test IfThenElse for bad operator and see result');
-$I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => 'bad'));
-$I->seeResult();
+$I->callResourceFromYaml(array('lhs' => 100, 'rhs' => 100, 'operator' => '>>'));
 $I->seeResponseCodeIs(400);
+$I->seeResponseContainsJson(["error" => ["code" => 1, "message" => "Invalid operator: >>.", "id" => 3]]);
 
 $I->tearDownTestFromYaml();
