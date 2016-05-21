375d3a1d356c53642a4946f6f32d2aced47c7bee
John Avery <laughing_man77@hotmail.com>
2016-05-21 21:00:02 +1000

    reactored Sort for changes in Object

--- includes/processor/Sort.php
+++ includes/processor/Sort.php
@@ -9,9 +9,6 @@ use Datagator\Core;
 
 class Sort extends ProcessorBase
 {
-  private $asc;
-  private $sortByValue;
-
   protected $details = array(
     'name' => 'Sort',
     'description' => 'Sort an input of multiple values. The values can be singular items or name/value pairs (sorted by key or value). Singular items cannot be mixed with name/value pairs.',
@@ -44,72 +41,24 @@ class Sort extends ProcessorBase
     if (!is_array($values) || empty($values)) {
       return $values;
     }
-    $this->asc = ($this->val($this->meta->direction) == 'asc');
-    $this->sortByValue = isset($this->meta->sortByValue) ? $this->val($this->meta->sortByValue) == 'true' : false;
 
-    Core\Debug::variable($this->sortByValue);
-    Core\Debug::variable($this->asc);
-    
-    $haveField = false;
-    $haveVal = false;
-    foreach ($values as $value) {
-      if (is_array($value)) {
-        if (sizeof($value) > 1) {
-          throw new Core\ApiException('invalid value found in sort - can only use single values or key/value pairs', 1, $this->id);
-        }
-        $haveField = true;
+    $asc = ($this->val($this->meta->direction) == 'asc');
+    $sortByValue = isset($this->meta->sortByValue) ? $this->val($this->meta->sortByValue) == 'true' : false;
+
+    if (!$sortByValue) {
+      if ($asc) {
+        ksort($values);
       } else {
-        $haveVal = true;
+        krsort($values);
       }
-    }
-
-    if ($haveVal && $haveField) {
-      throw new Core\ApiException('cannot sort a collection of mixed name/value pairs and values', 1, $this->id);
-    }
-    if ($haveField) {
-      usort($values, array($this, 'sortFields'));
-    } elseif ($this->asc) {
-      sort($values);
     } else {
-      rsort($values);
+      if ($asc) {
+        asort($values);
+      } else {
+        arsort($values);
+      }
     }
 
     return $values;
   }
-
-  /**
-   * @param $a
-   * @param $b
-   * @return int
-   * @throws \Datagator\Core\ApiException
-   */
-   public function sortFields($a, $b)
-   {
-     $ka = array_keys($a);
-     $kb = array_keys($b);
-     if ($this->sortByValue) {
-       $sa = $ka[0];
-       $sb = $kb[0];
-     } else {
-       $sa = $a[$ka[0]];
-       $sb = $b[$kb[0]];
-     }
-
-     if ($sa == $sb) {
-       return 0;
-     }
-     if (is_numeric($sa) && is_numeric($sb)) {
-       if (!$this->asc) {
-         return $sa > $sb ? +1 : -1;
-       } else {
-         return $sb > $sa ? +1 : -1;
-       }
-     } else {
-       if (!$this->asc) {
-         return strnatcmp($sa, $sb);
-       } else {
-         return strnatcmp($sb, $sa);
-       }
-     }
-   }
 }
