bc2cd02d2652b485007274f8cec76c06a8f9899b
John Avery <laughing_man77@hotmail.com>
2016-05-22 16:53:45 +1000

    tests for varFloat

---
+++ tests/api/VarFloatCept.php
@@ -0,0 +1,55 @@
+<?php 
+$I = new ApiTester($scenario);
+$I->performLogin();
+$I->setYamlFilename('varFloat.yaml');
+$I->createResourceFromYaml();
+
+$I->wantTo('populate a VarFloat with text and see the result.');
+$I->callResourceFromYaml(['value' => 'text']);
+$I->seeResponseCodeIs(417);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(["error" => ["code" => 6, "message" => "Invalid float: text.", "id" => 3]]);
+
+$I->wantTo('populate a VarFloat with true and see the result.');
+$I->callResourceFromYaml(['value' => 'true']);
+$I->seeResponseCodeIs(417);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(["error" => ["code" => 6, "message" => "Invalid float: true.", "id" => 3]]);
+
+$I->wantTo('populate a VarFloat with 1.6 and see the result.');
+$I->callResourceFromYaml(['value' => '1.6']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('1.6');
+
+$I->wantTo('populate a VarFloat with 1.6 and see the result.');
+$I->callResourceFromYaml(['value' => 1.6]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('1.6');
+
+$I->wantTo('populate a VarFloat with 1 and see the result.');
+$I->callResourceFromYaml(['value' => 1]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('1');
+
+$I->wantTo('populate a VarFloat with 1.0 and see the result.');
+$I->callResourceFromYaml(['value' => 1]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('1');
+
+$I->wantTo('populate a VarFloat with 0 and see the result.');
+$I->callResourceFromYaml(['value' => 0]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('0');
+
+$I->wantTo('populate a VarFloat with 0.0 and see the result.');
+$I->callResourceFromYaml(['value' => 0.0]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('0');
+
+$I->tearDownTestFromYaml();
