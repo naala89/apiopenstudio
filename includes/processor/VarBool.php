61b6a2fdd6ccaeca7a7be5ec6327336ac3983c11
John <3242482-john89@users.noreply.gitlab.com>
2016-05-22 15:59:57 +1000

    convert string from uri params to bool

--- includes/processor/VarBool.php
+++ includes/processor/VarBool.php
@@ -19,7 +19,7 @@ class VarBool extends VarMixed
 {
   protected $details = array(
     'name' => 'Var (Boolean)',
-    'description' => 'A boolean variable. It validates the input and returns an error if it is not a boolean.',
+    'description' => 'A boolean variable. It validates the input and returns an error if it is not a boolean. Possible input',
     'menu' => 'Primitive',
     'application' => 'All',
     'input' => array(
@@ -35,6 +35,10 @@ class VarBool extends VarMixed
   {
     Core\Debug::variable($this->meta, 'Processor VarBool', 4);
     $value = parent::process();
+
+    if (is_string($value)) {
+      $value = strtolower($value) == 'true' || $value = '1' || strtolower($value) =='yes';
+    }
 
     if (!is_bool($value)) {
       throw new Core\ApiException('invalid boolean', 5, $this->id, 417);
