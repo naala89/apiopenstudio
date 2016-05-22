5c7db582d405f731224c67f29d3723c1bcbbc183
John <3242482-john89@users.noreply.gitlab.com>
2016-05-22 18:08:03 +1000

    varMixed tests

--- tests/api/VarMixedCept.php
+++ tests/api/VarMixedCept.php
@@ -1,64 +1,64 @@
 <?php
 $I = new ApiTester($scenario);
 $I->performLogin();
-$I->setYamlFilename('varNum.yaml');
+$I->setYamlFilename('varMixed.yaml');
 $I->createResourceFromYaml();
 
-$I->wantTo('populate a varNum with text and see the result.');
+$I->wantTo('populate a varMixed with text and see the result.');
 $I->callResourceFromYaml(['value' => 'text']);
-$I->seeResponseCodeIs(417);
+$I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(["error" => ["code" => 6, "message" => "Invalid number: text.", "id" => 3]]);
+$I->seeResponseContains('text');
 
-$I->wantTo('populate a varNum with true and see the result.');
+$I->wantTo('populate a varMixed with true and see the result.');
 $I->callResourceFromYaml(['value' => 'true']);
-$I->seeResponseCodeIs(417);
+$I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(["error" => ["code" => 6, "message" => "Invalid number: true.", "id" => 3]]);
+$I->seeResponseContains('true');
 
-$I->wantTo('populate a varNum with 1.6 and see the result.');
+$I->wantTo('populate a varMixed with 1.6 and see the result.');
 $I->callResourceFromYaml(['value' => '1.6']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1.6');
 
-$I->wantTo('populate a varNum with 1.6 and see the result.');
+$I->wantTo('populate a varMixed with 1.6 and see the result.');
 $I->callResourceFromYaml(['value' => 1.6]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1.6');
 
-$I->wantTo('populate a varNum with 1 and see the result.');
+$I->wantTo('populate a varMixed with 1 and see the result.');
 $I->callResourceFromYaml(['value' => 1]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1');
 
-$I->wantTo('populate a varNum with 1.0 and see the result.');
+$I->wantTo('populate a varMixed with 1.0 and see the result.');
 $I->callResourceFromYaml(['value' => 1.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1');
 
-$I->wantTo('populate a varNum with -11 and see the result.');
+$I->wantTo('populate a varMixed with -11 and see the result.');
 $I->callResourceFromYaml(['value' => -11]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('-11');
 
-$I->wantTo('populate a varNum with -11.0 and see the result.');
+$I->wantTo('populate a varMixed with -11.0 and see the result.');
 $I->callResourceFromYaml(['value' => -11.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('-11');
 
-$I->wantTo('populate a varNum with 0 and see the result.');
+$I->wantTo('populate a varMixed with 0 and see the result.');
 $I->callResourceFromYaml(['value' => 0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('0');
 
-$I->wantTo('populate a varNum with 0.0 and see the result.');
+$I->wantTo('populate a varMixed with 0.0 and see the result.');
 $I->callResourceFromYaml(['value' => 0.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
