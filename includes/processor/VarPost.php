a64f5363ab8d3cbc20ed9a85797819ab11ad7678
John Avery <laughing_man77@hotmail.com>
2016-05-23 10:38:39 +1000

    testing for VarPost and fix tests for wrobg varname

--- includes/processor/VarPost.php
+++ includes/processor/VarPost.php
@@ -10,7 +10,7 @@ use Datagator\Core;
 class VarPost extends VarMixed
 {
   protected $details = array(
-    'name' => 'Var (Get)',
+    'name' => 'Var (Post)',
     'description' => 'A "post" variable. It fetches a variable from the post request.',
     'menu' => 'Primitive',
     'application' => 'All',
@@ -18,23 +18,20 @@ class VarPost extends VarMixed
       'name' => array(
         'description' => 'The name of the variable.',
         'cardinality' => array(1, 1),
-        'accepts' => array('processor', 'string')),
+        'accepts' => array('processor', 'string')
+      ),
     ),
   );
 
-  /**
-   * @return mixed
-   * @throws \Datagator\Core\ApiException
-   * @throws \Datagator\Processor\ApiException
-   */
   public function process()
   {
     Core\Debug::variable($this->meta, 'Processor VarPost', 4);
     $name = $this->val($this->meta->name);
 
-    if (!empty($this->request->vars[$name])) {
+    if (isset($this->request->vars[$name])) {
       return $this->request->vars[$name];
     }
+
     return null;
   }
 }
--- tests/api/VarPostCept.php
+++ tests/api/VarPostCept.php
@@ -1,58 +1,58 @@
 <?php
 $I = new ApiTester($scenario);
 $I->performLogin();
-$I->setYamlFilename('varGet.yaml');
+$I->setYamlFilename('varPost.yaml');
 $I->createResourceFromYaml();
 
-$I->wantTo('populate a VarGet with text and see the result.');
+$I->wantTo('populate a varPost with text and see the result.');
 $I->callResourceFromYaml(['value' => 'text']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('text');
 
-$I->wantTo('populate a VarGet with true and see the result.');
+$I->wantTo('populate a varPost with true and see the result.');
 $I->callResourceFromYaml(['value' => 'true']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('true');
 
-$I->wantTo('populate a VarGet with 1.6 and see the result.');
+$I->wantTo('populate a varPost with 1.6 and see the result.');
 $I->callResourceFromYaml(['value' => '1.6']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1.6');
 
-$I->wantTo('populate a VarGet with 1.6 and see the result.');
+$I->wantTo('populate a varPost with 1.6 and see the result.');
 $I->callResourceFromYaml(['value' => 1.6]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1.6');
 
-$I->wantTo('populate a VarGet with 1 and see the result.');
+$I->wantTo('populate a varPost with 1 and see the result.');
 $I->callResourceFromYaml(['value' => 1.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1');
 
-$I->wantTo('populate a VarGet with 1.0 and see the result.');
+$I->wantTo('populate a varPost with 1.0 and see the result.');
 $I->callResourceFromYaml(['value' => 1]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('1');
 
-$I->wantTo('populate a VarGet with 0 and see the result.');
+$I->wantTo('populate a varPost with 0 and see the result.');
 $I->callResourceFromYaml(['value' => 0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('0');
 
-$I->wantTo('populate a VarGet with 0.0 and see the result.');
+$I->wantTo('populate a varPost with 0.0 and see the result.');
 $I->callResourceFromYaml(['value' => 0.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('0');
 
-$I->wantTo('populate a VarGet with wrong varname and see the result.');
+$I->wantTo('populate a varPost with wrong varname and see the result.');
 $I->callResourceFromYaml(['values' => 'test']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
