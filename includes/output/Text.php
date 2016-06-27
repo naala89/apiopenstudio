8280ce29dcf39bd542d05a3b4513c8acb998a90f
John Avery <laughing_man77@hotmail.com>
2016-06-27 13:27:41 +1000

    incorrect details in outputs

--- includes/output/Text.php
+++ includes/output/Text.php
@@ -24,7 +24,7 @@ class Text extends Output
       'options' => array(
         'description' => 'Extra Curl options to be applied when sent to the destination  (e.g. cursor: -1, screen_name: foobarapi, skip_status: true, etc).',
         'cardinality' => array(0, '*'),
-        'accepts' => array('processor field'),
+        'accepts' => array('function field'),
       ),
     ),
   );
