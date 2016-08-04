9b5cbd56e710c48605dff9e1fb1241ab68ca718e
John <3242482-john89@users.noreply.gitlab.com>
2016-08-04 10:25:48 +1000

    processorsAll

--- tests/api/ProcessorsCept.php
+++ tests/api/ProcessorsCept.php
@@ -9,6 +9,7 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseMatchesJsonType([
   'name' => 'string',
+  'machineName' => 'string',
   'description' => 'string',
   'application' => 'string',
   'input' => 'array',
@@ -37,36 +38,82 @@ $I->seeResponseMatchesJsonType([
     }
   },
 */
-foreach (\GuzzleHttp\json_decode($I->getResponse()) as $processor) {
-  foreach ($processor->input as $input) {
-    if (empty($inputDetails->name)) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing a description on its input: $inputName");
+foreach (\GuzzleHttp\json_decode($I->getResponse()) as $index => $processor) {
+  if (empty($processor->name)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $index . ' is missing a name in its details in its details.');
+  }
+  if (empty($processor->machineName)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' is missing a machineName in its details.');
+  }
+  if (empty($processor->description)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' is missing a description in its details.');
+  }
+  if (empty($processor->menu)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' is missing a menu in its details.');
+  }
+  if (empty($processor->application)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' is missing an application in its details.');
+  }
+  if (!isset($processor->input)) {
+    \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' is missing an input in its details.');
+  }
+  foreach ($processor->input as $key => $val) {
+    if (is_numeric($key)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " index must be a textual name for input ($key)");
     }
-    /*
-    if (empty($inputDetails->cardinality)) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing cardinality on its input: $inputName");
+    if (empty($val->description)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a description for input input ($key)");
     }
-    if (!isset($inputDetails->cardinality[0])) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing min cardinality on its input: $inputName");
+    if (!isset($val->cardinality)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing cardinality on its input: $key");
     }
-    if (!is_integer($inputDetails->cardinality[0] + 0)) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' min cardinality must be an integer: ' . $inputDetails->cardinality[0]);
+    if (!is_array($val->cardinality)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " must have cardinality of type array on its input: $key");
     }
-    if ($inputDetails->cardinality[0] < 0) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' min cardinality must be a positive value: ' . $inputDetails->cardinality[0]);
+    if (!isset($val->cardinality[0])) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing min cardinality on its input: $key");
     }
-    if (!isset($inputDetails->cardinality[1])) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing max cardinality on its input: $inputName");
+    if (!is_integer($val->cardinality[0] + 0)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " min cardinality must be an integer on its input: $key");
     }
-    if (!is_integer($inputDetails->cardinality[1] + 0) && $inputDetails->cardinality[1] != '*') {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' max cardinality must be an integer or "*": ' . $inputDetails->cardinality[1]);
+    if ($val->cardinality[0] < 0) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " min cardinality must be a positive value on its input: $key");
     }
-    if (is_integer($inputDetails->cardinality[1] + 0) && $inputDetails->cardinality[1] < 0) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . ' max cardinality must be a positive value: ' . $inputDetails->cardinality[1]);
+    if (!isset($val->cardinality[1])) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing max cardinality on its input: $key");
     }
-    if (empty($inputDetails->accepts)) {
-      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " is missing an accepts on its input: $inputName");
+    if (!is_integer($val->cardinality[1] + 0) && $val->cardinality[1] != '*') {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " max cardinality must be an integer or "*" on its input: $key");
     }
-    */
+    if (is_integer($val->cardinality[1] + 0) && $val->cardinality[1] < 0) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " max cardinality must be a positive value on its input: $key");
+    }
+    if (!isset($val->literalAllowed)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a literalAllowed  on its input: $key");
+    }
+    if (!is_bool($val->literalAllowed)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " literalAllowed must be a boolean on its input: $key");
+    }
+    if (!isset($val->limitFunctions)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a limitFunctions  on its input: $key");
+    }
+    if (!isset($val->limitTypes)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a limitTypes  on its input: $key");
+    }
+    foreach ($val->limitTypes as $limitType) {
+      $limitTypes = array('file', 'object', 'string', 'number', 'integer', 'float', 'boolean');
+      if (!in_array($limitType, $limitTypes)) {
+        \PHPUnit_Framework_Assert::assertTrue(FALSE, 'the processor: ' . $processor->name . " can only have a value of " . implode(',', $limitTypes) . " on its limitTypes: $key");
+      }
+    }
+    if (!isset($val->limitValues)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a limitValues on its input: $key");
+    }
+    if (!is_array($val->limitValues)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " limitValues must be an array on its input: $key");
+    }
+    if (!isset($val->default)) {
+      \PHPUnit_Framework_Assert::assertTrue(false,  'the processor: ' . $processor->name . " needs a default on its input: $key");
+    }
   }
 }
