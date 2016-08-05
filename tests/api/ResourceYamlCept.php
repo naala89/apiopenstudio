dd027f62325ea39e806332b7c48b4e32c5f7d13d
John Avery <laughing_man77@hotmail.com>
2016-08-05 17:17:02 +1000

    resourceYaml

--- tests/api/ResourceYamlCept.php
+++ tests/api/ResourceYamlCept.php
@@ -55,7 +55,7 @@ $I->seeResponseIsJson();
 $I->seeResponseContainsJson(['error' => ['code' => 6, 'message' => 'Missing or negative ttl in new resource.', 'id' => -1]]);
 $I->setYamlFilename('resourceTtl-1.yaml');
 $I->tearDownTestFromYaml(400, ['error' => ['code' => 2,'message' => 'Could not delete resource, not found.', 'id' => -1]]);
-/*
+
 $I->wantTo('create a new resource from YAML missing security attr and see the result');
 $I->sendPOST($uri, ['token' => $I->getMyStoredToken()], ['resource' => 'tests/_data/resourceNoSecurity.yaml']);
 $I->seeResponseCodeIs(200);
@@ -215,4 +215,3 @@ $I->seeResponseIsJson();
 $I->seeResponseContainsJson(["error" => ["code" => 6, "message" => "This resource is reserved (get + processors/all).", "id" => -1]]);
 $I->setYamlFilename('ResourceReserved.yaml');
 $I->tearDownTestFromYaml(400, ['error' => ['code' => 2,'message' => 'Could not delete resource, not found.', 'id' => -1]]);
-*/
