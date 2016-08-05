62da88cbd027e24aa94592d63f5f061567ceb55d
John <3242482-john89@users.noreply.gitlab.com>
2016-08-05 17:13:34 +1000

    fragments

--- tests/api/FragmentsCept.php
+++ tests/api/FragmentsCept.php
@@ -6,7 +6,7 @@ $I->haveHttpHeader('Accept', 'application/json');
 $I->wantTo('concatenate values from fragments and see the result');
 $I->setYamlFilename('fragment.yaml');
 $I->createResourceFromYaml();
-$uri = '/' . $I->getMyApplicationName() . '/test/fragments';
+$uri = '/' . $I->getMyApplicationName() . '/fragments';
 $I->sendGet($uri . '/test/fragments', ['token' => $I->getMyStoredToken(), 'val1' => 1, 'val2' => 2, 'val3' => 3]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
