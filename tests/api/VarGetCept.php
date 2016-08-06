88291aee48620a27ab2011e4e92efff677bf2a79
John Avery <laughing_man77@hotmail.com>
2016-08-06 10:31:20 +1000

    VarGet

--- tests/api/VarGetCept.php
+++ tests/api/VarGetCept.php
@@ -52,10 +52,23 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('0');
 
-$I->wantTo('populate a VarGet with wrong varname and see the result.');
-$I->callResourceFromYaml(['values' => 'test']);
+$I->wantTo('populate a VarGet with wrong varname and nullable true and see the result.');
+$I->callResourceFromYaml(['values' => 'test', 'nullable' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('null');
+$I->seeResponseEquals('');
+
+$I->wantTo('populate a VarGet with wrong varname and nullable false and see the result.');
+$I->callResourceFromYaml(['values' => 'test', 'nullable' => false]);
+$I->callResourceFromYaml(['values' => 'test']);
+$I->seeResponseCodeIs(417);
+$I->seeResponseIsJson();
+$I->seeResponseContains('Post variable (value) not received.');
+
+$I->wantTo('populate a VarGet with wrong varname and nullable not set and see the result.');
+$I->callResourceFromYaml(['values' => 'test']);
+$I->seeResponseCodeIs(417);
+$I->seeResponseIsJson();
+$I->seeResponseContains('Post variable (value) not received.');
 
 $I->tearDownTestFromYaml();
