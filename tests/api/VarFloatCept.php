0b6cce984c7e1f1e8e3ec0c6efacc506b43f87fa
John <3242482-john89@users.noreply.gitlab.com>
2016-08-23 12:33:46 +1000

    fix last of var processors

--- tests/api/VarFloatCept.php
+++ tests/api/VarFloatCept.php
@@ -44,7 +44,7 @@ $I->wantTo('populate a VarFloat with 0 and see the result.');
 $I->callResourceFromYaml(['value' => 0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('false');
+$I->seeResponseContains('0');
 
 $I->wantTo('populate a VarFloat with 0.0 and see the result.');
 $I->callResourceFromYaml(['value' => 0.0]);
