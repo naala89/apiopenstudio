fb51168248b0f17fae2b9814933277646e58e7a5
John Avery <laughing_man77@hotmail.com>
2016-10-09 13:21:45 +1100

    ObjectProcessorCept

--- tests/api/ObjectProcessorCept.php
+++ tests/api/ObjectProcessorCept.php
@@ -8,5 +8,5 @@ $I->tearDownTestFromYaml();
 $I = new ApiTester($scenario);
 $I->wantTo('create an Object processor of fields and literals and see result');
 $I->doTestFromYaml('objectFieldsTest.yaml');
-$I->seeResponseContainsJson(["field1","field2","field3","myTestVar" => "MyTestVal"]);
+$I->seeResponseContainsJson(["field1","field2","field3",["myTestVar" => "MyTestVal"]]);
 $I->tearDownTestFromYaml();
