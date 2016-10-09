6b92cc8288eb28de2316a3d1784840d8155abe41
John Avery <laughing_man77@hotmail.com>
2016-10-09 11:22:25 +1100

    doco

--- includes/Core/ApiException.php
+++ includes/Core/ApiException.php
@@ -8,23 +8,33 @@ class ApiException extends \Exception
   private $htmlCode;
 
   /**
+   * Throw an API exception. This will return a standard error object in the format requested in the header.
    * @param string $message
    * @param int $code
-   * @param int $processor
+   * @param string|int $processor
    * @param int $htmlCode
    * @param \Exception|NULL $previous
    */
-  public function __construct($message, $code = 0, $processor = -1, $htmlCode = 400, \Exception $previous = null) {
+  public function __construct($message, $code = 0, $processor = -1, $htmlCode = 400, \Exception $previous = null)
+  {
     $this->processor = $processor;
     $this->htmlCode = $htmlCode;
     parent::__construct($message, $code, $previous);
   }
 
+  /**
+   * Get the ID of the processor.
+   * @return int|string
+   */
   public function getProcessor()
   {
     return $this->processor;
   }
 
+  /**
+   * Get the HTML return code.
+   * @return int
+   */
   public function getHtmlCode()
   {
     return $this->htmlCode;
