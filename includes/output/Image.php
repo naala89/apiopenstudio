0ecc3434324af1b8b1091578e4416d8e04493719
John Avery <laughing_man77@hotmail.com>
2016-12-21 10:32:16 +1100

    remove old dev work

--- includes/output/Image.php
+++ includes/output/Image.php
@@ -48,39 +48,6 @@ class Image extends Output
       ),
     ),
   );
-/*
-  public function process()
-  {
-    parent::process();
-
-    if (!is_string($this->data)) {
-      header('Content-Type: application/json');
-      throw new Core\ApiException('data revieved is not an image.', 1, $this->id, 406);
-    }
-    if (empty($this->data)) {
-      throw new Core\ApiException('image empty.', 1, $this->id);
-    }
-
-    if (substr($this->data, 0, 4 ) === "http") {
-      $curl = new Core\Curl();
-      $image = $curl->get($this->data, array('CURLOPT_SSL_VERIFYPEER' => 0, 'CURLOPT_FOLLOWLOCATION' => 1));
-      header('Content-Type:' . $curl->type);
-      return $image;
-    }
-
-    if (function_exists('finfo_open')) {
-      $finfo = finfo_open(FILEINFO_MIME_TYPE);
-      $mime = finfo_file($finfo, $this->data);
-    } elseif (function_exists('mime_content_type')) {
-      $mime = mime_content_type($this->data);
-    } else {
-      throw new Core\ApiException('Cannot read mime type of image. Please enable filetype extension.', 1, $this->id);
-    }
-
-    header("Content-Type:$mime");
-    return file_get_contents($this->data);
-  }
-*/
 
   protected function fromXml(& $data) {
     return 'data is not an image';
