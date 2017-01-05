0ab4eb3871942abac6c85078bc0151f2e294c6a6
John Avery <laughing_man77@hotmail.com>
2017-01-05 11:47:10 +1100

    filterObjectNonRegex

--- tests/api/FilterCept.php
+++ tests/api/FilterCept.php
@@ -3,86 +3,60 @@ $I = new ApiTester($scenario);
 $I->performLogin();
 $I->haveHttpHeader('Accept', 'application/json');
 
-/**
- * filter by key
- */
-
-$I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, inverse, non-recursive');
-$I->setYamlFilename('filterObject.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => false, 'inverse' => false, 'recursive' => false, 'strict' => false]);
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson([2 => ['key3' => 'val3'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
-/*
-$I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, inverse, recursive');
-$I->setYamlFilename('filterFieldsKeyNonregexInverseRecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
-
 $I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, non-inverse, non-recursive');
-$I->setYamlFilename('filterFieldsKeyNonregexNoninverseNonrecursive.yaml');
+$I->setYamlFilename('filterObjectNonRegex.yaml');
 $I->createResourceFromYaml();
-$I->callResourceFromYaml();
+$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => false, 'inverse' => false, 'recursive' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
+$I->seeResponseContainsJson(['key2' => 'val2', 'key3' => 'val3', 'key5' => ['key6' => 'val6', 'key7' => 'val7']]);
 
 $I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, non-inverse, recursive');
-$I->setYamlFilename('filterFieldsKeyNonregexNoninverseRecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
+$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => false, 'inverse' => false, 'recursive' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
+$I->seeResponseContainsJson(['key2' => 'val2', 'key3' => 'val3', 'key5' => ['key7' => 'val7']]);
 
-$I->wantTo('Test Filter by filtering by key on an array of fields with settings regex, inverse, non-recursive');
-$I->setYamlFilename('filterFieldsKeyRegexInverseNonrecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
+$I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, inverse, non-recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => false, 'inverse' => true, 'recursive' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
+$I->seeResponseContainsJson(['key1' => 'val1', 'key4' => 'val4']);
 
-$I->wantTo('Test Filter by filtering by key on an array of fields with settings regex, inverse, recursive');
-$I->setYamlFilename('filterFieldsKeyRegexInverseRecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
+$I->wantTo('Test Filter by filtering by key on an array of fields with settings non-regex, inverse, recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => false, 'inverse' => true, 'recursive' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
+$I->seeResponseContainsJson(['key1' => 'val1', 'key4' => 'val4']);
 
 $I->wantTo('Test Filter by filtering by key on an array of fields with settings regex, non-inverse, non-recursive');
-$I->setYamlFilename('filterFieldsKeyRegexNoninverseNonrecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
-$I->seeResponseCodeIs(200);
+$I->callResourceFromYaml(['keyOrValue' => 'key', 'regex' => true, 'inverse' => false, 'recursive' => false]);
+$I->seeResponseCodeIs(417);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
+$I->seeResponseContainsJson(["error" => ["id" => 3, "code" => 0, "message" => "Cannot have an array of regexes as a filter."]]);
 
-$I->wantTo('Test Filter by filtering by key on an array of fields with settings regex, non-inverse, recursive');
-$I->setYamlFilename('filterFieldsKeyRegexNoninverseRecursive.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml();
+$I->wantTo('Test Filter by filtering by value on an array of fields with settings non-regex, non-inverse, non-recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'value', 'regex' => false, 'inverse' => false, 'recursive' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResult();
-$I->seeResponseContainsJson([0 => ['key1' => 'val1'], 1 => ['key2' => 'val2'], 2 => ['key3' => 'val3'], 3 => ['key4' => 'val4'], 4 => ['key5' => 'val5']]);
-//$I->tearDownTestFromYaml();
-*/
+$I->seeResponseContainsJson(['key1' => 'val1', 'key2' => 'val2', 'key4' => 'val4', 'key5' => ['key6' => 'val6', 'key7' => 'val7']]);
+
+$I->wantTo('Test Filter by filtering by value on an array of fields with settings non-regex, non-inverse, recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'value', 'regex' => false, 'inverse' => false, 'recursive' => true]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['key1' => 'val1', 'key2' => 'val2', 'key4' => 'val4', 'key5' => ['key6' => 'val6']]);
+
+$I->wantTo('Test Filter by filtering by value on an array of fields with settings non-regex, inverse, non-recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'value', 'regex' => false, 'inverse' => true, 'recursive' => false]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['key3' => 'val3']);
+
+$I->wantTo('Test Filter by filtering by value on an array of fields with settings non-regex, inverse, recursive');
+$I->callResourceFromYaml(['keyOrValue' => 'value', 'regex' => false, 'inverse' => true, 'recursive' => true]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['key3' => 'val3']);
+
+$I->tearDownTestFromYaml();
