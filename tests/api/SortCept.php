23f1840fb16e33ed3db84932742e58282f3e28f3
John Avery <laughing_man77@hotmail.com>
2017-01-06 17:26:57 +1100

    fixed up Sort and tests, also implemented Collection

--- tests/api/SortCept.php
+++ tests/api/SortCept.php
@@ -2,10 +2,10 @@
 $I = new ApiTester($scenario);
 $I->performLogin();
 
-$I->wantTo('Sort an array of single values in ascending order and see the result.');
-$I->setYamlFilename('sortArrayOfSingular.yaml');
+$I->wantTo('Sort a Collection by value in ascending order and see the result.');
+$I->setYamlFilename('sortCollection.yaml');
 $I->createResourceFromYaml();
-$I->callResourceFromYaml(['direction' => 'asc']);
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
@@ -15,57 +15,99 @@ $I->seeResponseContainsJson([
   "field4",
   1,
   5]);
-$I->tearDownTestFromYaml();
 
-$I->wantTo('Sort an array of mixed values of single and Field in ascending order and see the result.');
-$I->setYamlFilename('sortArrayOfSingularWithField.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml(['direction' => 'asc']);
+$I->wantTo('Sort a Collection by value in descending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
-  'field1',
-  'field2',
-  'field3',
-  'field4',
-  ['key' => 'value'],
+  5,
+  1,
+  "field4",
+  "field3",
+  "field2",
+  "field1"]);
+
+$I->wantTo('Sort a Collection by key in ascending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  "field1",
+  "field4",
+  "field2",
+  "field3",
+  1,
   5]);
+
+$I->wantTo('Sort a Collection by key in descending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  5,
+  1,
+  "field3",
+  "field2",
+  "field4",
+  "field1"]);
 $I->tearDownTestFromYaml();
 
-$I->wantTo('Sort an array of mixed values of single and processors in ascending order and see the result.');
-$I->setYamlFilename('sortArrayOfSingularAndProcessors.yaml');
+$I->wantTo('Sort multiple values by value in ascending order and see the result.');
+$I->setYamlFilename('sortMultiple.yaml');
 $I->createResourceFromYaml();
-$I->callResourceFromYaml(['direction' => 'asc']);
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
   "field1",
   "field2",
   "field3",
-  "testStr",
+  "field4",
   1,
-  3.4562599999999999,
-  5
-]);
+  5]);
 
-$I->tearDownTestFromYaml();
-
-$I->wantTo('Sort an Object of Fields by key in ascending order and see the result.');
-$I->setYamlFilename('sortObjectOfFields.yaml');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml(['direction' => 'asc', 'sortByValue' => 'false']);
+$I->wantTo('Sort multiple values by value in descending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
-  'key1' => 'val99',
-  'key2' => 'val5',
-  'key3' => 'val3',
-  'key4' => 'val2',
-  'key999' => 'val1'
-]);
+  5,
+  1,
+  "field4",
+  "field3",
+  "field2",
+  "field1"]);
 
-$I->wantTo('Sort an Object of Fields by key in descending order and see the result.');
-$I->callResourceFromYaml(['direction' => 'desc', 'sortByValue' => 'false']);
+$I->wantTo('Sort multiple values by key in ascending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  "field1",
+  "field4",
+  "field2",
+  "field3",
+  1,
+  5]);
+
+$I->wantTo('Sort multiple values by key in descending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  5,
+  1,
+  "field3",
+  "field2",
+  "field4",
+  "field1"]);
+$I->tearDownTestFromYaml();
+
+$I->wantTo('Sort an Object by value in ascending order and see the result.');
+$I->setYamlFilename('sortObject.yaml');
+$I->createResourceFromYaml();
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
@@ -73,19 +115,41 @@ $I->seeResponseContainsJson([
   'key4' => 'val2',
   'key3' => 'val3',
   'key2' => 'val5',
-  'key1' => 'val99',
-]);
+  'key1' => 'val99']);
 
-$I->wantTo('Sort an Object of Fields by value in descending order and see the result.');
-$I->callResourceFromYaml(['direction' => 'desc', 'sortByValue' => 'true']);
+$I->wantTo('Sort an Object by value in descending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'value']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
-  "key1" => "val99",
-  "key2" => "val5",
-  "key3" => "val3",
-  "key4" => "val2",
-  "key999" => "val1"
-]);
+  'key1' => 'val99',
+  'key2' => 'val5',
+  'key3' => 'val3',
+  'key4' => 'val2',
+  'key999' => 'val1']);
+
+$I->wantTo('Sort an Object by key in asscending order and see the result.');
+$I->callResourceFromYaml(['direction' => 'asc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  'key1' => 'val99',
+  'key2' => 'val5',
+  'key3' => 'val3',
+  'key4' => 'val2',
+  'key999' => 'val1']);
+
+$I->wantTo('Sort an Object by key in descending order and see the result.');
+$I->setYamlFilename('sortObject.yaml');
+$I->createResourceFromYaml();
+$I->callResourceFromYaml(['direction' => 'desc', 'sortby' => 'key']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+  'key999' => 'val1',
+  'key4' => 'val2',
+  'key3' => 'val3',
+  'key2' => 'val5',
+  'key1' => 'val99']);
 
 $I->tearDownTestFromYaml();
