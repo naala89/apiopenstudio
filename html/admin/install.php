8f5f6c742fc5203d95118cb4522dd8dccc50623e
John Avery <laughing_man77@hotmail.com>
2018-08-04 15:52:52 +1000

    db successfully populated

--- html/admin/install.php
+++ html/admin/install.php
@@ -44,6 +44,10 @@ switch ($step) {
   case 1:
     $yaml = file_get_contents(Config::$dbBase);
     $definition = \Spyc::YAMLLoadString($yaml);
+    $message = [
+      'type' => 'info',
+      'text' => 'Creating database tables...<br />'
+    ];
 
     foreach ($definition as $table => $tableData) {
       $sqlPrimary = '';
@@ -52,18 +56,44 @@ switch ($step) {
         $sqlColumn = "`$column` ";
         $sqlColumn .= ' ' . $columnData['type'];
         $sqlColumn .= isset($columnData['notnull']) && $columnData['notnull'] ? ' NOT NULL' : '';
+        $sqlColumn .= isset($columnData['default']) ? (' DEFAULT ' . $columnData['default']) : '';
         $sqlColumn .= isset($columnData['autoincrement']) ? ' AUTO_INCREMENT' : '';
         $sqlColumn .= isset($columnData['primary']) ? ' PRIMARY KEY' : '';
         $sqlColumn .= isset($columnData['comment']) ? (" COMMENT '" . $columnData['comment'] . "'") : '';
         $sqlColumns[] = $sqlColumn;
       }
       $sqlCreate = "CREATE TABLE IF NOT EXISTS `$table` (" . implode(', ', $sqlColumns) . ');';
-      $result = $db->execute($sqlCreate);
+      if (empty($db->execute($sqlCreate))) {
+        $message['text'] .= "Create `$table` fail!<br />";
+        $message['type'] = 'error';
+        echo $template->render(['message' => $message, 'menu' => $menu]);
+        exit;
+      } else {
+        $message['text'] .= "Create `$table` success!<br />";
+      }
+      $sqlTruncate = "TRUNCATE `$table`;";
+      $db->execute($sqlTruncate);
+      if (isset($tableData['data'])) {
+        foreach ($tableData['data'] as $row) {
+          $keys = [];
+          $values = [];
+          foreach ($row as $key => $value) {
+            $keys[] = $key;
+            $values[] = is_string($value) ? "\"$value\"" : $value;
+          }
+          $sqlRow = "INSERT INTO `$table` (" . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ');';
+          if (empty($db->execute($sqlRow))) {
+            $message['text'] .= "Populate `$table` fail!<br />";
+            $message['text'] .= "Processing halted. Please check the logs.";
+            $message['type'] = 'error';
+            echo $template->render(['message' => $message, 'menu' => $menu]);
+            exit;
+          }
+        }
+        $message['text'] .= "Populate `$table` success!<br />";
+      }
     }
-    $message = [
-      'type' => 'info',
-      'text' => 'DB created!.'
-    ];
+    $message['text'] .= "Database Successfully created!";
     echo $template->render(['message' => $message, 'menu' => $menu]);
     exit;
 }
