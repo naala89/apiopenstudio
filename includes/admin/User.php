cec18ebbfc750002a00771816df64fd49f576360
John <3242482-john89@users.noreply.gitlab.com>
2018-08-08 06:39:38 +1000

    phpdoc in User

--- includes/admin/User.php
+++ includes/admin/User.php
@@ -15,6 +15,14 @@ class User
     $_SESSION['token'] = '';
   }
 
+  /**
+   * Log a user in.
+   *
+   * @param $username
+   * @param $password
+   *
+   * @return bool|mixed
+   */
   public function login($username, $password)
   {
     $payload = ['form_params' => [
@@ -36,17 +44,48 @@ class User
     return FALSE;
   }
 
+  /**
+   * Log a user out.
+   *
+   * @return bool
+   */
   public function logout()
   {
     $_SESSION['token'] = '';
     return TRUE;
   }
 
+  /**
+   * Check if a user is logged in.
+   *
+   * @return bool
+   */
   public function isLoggedIn()
   {
     return $_SESSION['token'] != '';
   }
 
+  /**
+   * Create a user.
+   *
+   * @param $username
+   * @param $password
+   * @param null $email
+   * @param null $honorific
+   * @param null $nameFirst
+   * @param null $nameLast
+   * @param null $company
+   * @param null $website
+   * @param null $addressStreet
+   * @param null $addressSuburb
+   * @param null $addressCity
+   * @param null $addressState
+   * @param null $addressPostcode
+   * @param null $phoneMobile
+   * @param null $phoneWork
+   *
+   * @return bool|int
+   */
   public function create($username, $password, $email=NULL, $honorific=NULL, $nameFirst=NULL, $nameLast=NULL, $company=NULL, $website=NULL, $addressStreet=NULL, $addressSuburb=NULL, $addressCity=NULL, $addressState=NULL, $addressPostcode=NULL, $phoneMobile=NULL, $phoneWork=NULL)
   {
     $user = new Db\User(
