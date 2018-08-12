21cef575e99c5d3b7689f0f1fa1eb0400b84b799
John Avery <laughing_man77@hotmail.com>
2018-08-13 08:39:19 +1000

    added login route and template

--- includes/Admin/routes.php
+++ includes/Admin/routes.php
@@ -4,16 +4,18 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Datagator\Admin;
 
+$app->get('/login', function (Request $request, Response $response) {
+  return $this->get('view')->render($response, 'login.twig', []);
+});
+
 $app->get('/', function (Request $request, Response $response) {
   $response->getBody()->write("It works! This is the default welcome page.");
-
   return $response;
-})->add(new Admin\Middleware\Authentication($settings));
+})->add(new Admin\Middleware\Authentication($settings, '/login'));
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
   $name = $request->getAttribute('name');
   $response->getBody()->write("Hello, $name");
-
   return $response;
 });
 
@@ -21,7 +23,5 @@ $app->get('/time', function (Request $request, Response $response) {
   $viewData = [
     'now' => date('Y-m-d H:i:s')
   ];
-
-
   return $this->get('view')->render($response, 'time.twig', $viewData);
-})->add(new Admin\Middleware\Authentication($settings));
+});
