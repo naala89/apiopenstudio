6a3434a866683970c739d38267bb775ec2ca86ad
John <3242482-john89@users.noreply.gitlab.com>
2018-08-15 22:59:55 +1000

    edit application

--- html/admin/js/main.js
+++ html/admin/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   M.AutoInit();
+
+  // Edit application modal
+  $('.modal-app-edit').click(function() {
+    var self = $(this);
+    var modal = $('#modal-edit');
+    modal.find('#edit-app-id').val(self.attr('app-id'));
+    modal.find('#edit-app-name').val(self.attr('app-name'));
+    modal.modal('open');
+  });
 });
