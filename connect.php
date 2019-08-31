757f90266d176700f3e22ebcbf98ac83d3841929
John.Avery <john.avery@adg.com.au>
2019-08-31 12:02:38 +1000

    php with nginx, no sql

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = 'gaterdata_mysql';
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+ 
+$conn = mysqli_connect($host, $user, $pass);
+if (!$conn) {
+    exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+}
+ 
+echo 'Successful database connection!'.PHP_EOL;
