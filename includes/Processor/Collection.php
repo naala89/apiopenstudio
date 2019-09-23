9734854b787ae31e65a954e4e23481844c9b6929
John <3242482-john89@users.noreply.gitlab.com>
2019-09-24 07:51:41 +1000

    Refactor Collection processor - untested.

--- includes/Processor/Collection.php
+++ includes/Processor/Collection.php
@@ -1,32 +1,33 @@
 <?php
 
-/**
- *
- */
-
 namespace Gaterdata\Processor;
+
 use Gaterdata\Core;
+
+/**
+ * Defines the Collection class.
+ */
 
 class Collection extends Core\ProcessorEntity
 {
-  protected $details = array(
+  protected $details = [
     'name' => 'Collection',
     'machineName' => 'collection',
-    'description' => 'Collection contains multiple values, like an array.',
+    'description' => 'Collection contains multiple values, like an array or list.',
     'menu' => 'Primitive',
     'application' => 'Common',
-    'input' => array(
-      'values' => array(
+    'input' => [
+      'values' => [
         'description' => 'The values in the collection',
-        'cardinality' => array(0, '*'),
+        'cardinality' => [0, '*'],
         'literalAllowed' => true,
-        'limitFunctions' => array(),
-        'limitTypes' => array(),
-        'limitValues' => array(),
+        'limitFunctions' => [],
+        'limitTypes' => [],
+        'limitValues' => [],
         'default' => ''
-      ),
-    ),
-  );
+      ],
+    ],
+  ];
 
   public function process()
   {
@@ -34,20 +35,26 @@ class Collection extends Core\ProcessorEntity
 
     $values = $this->val('values', true);
 
+    if ($this->isDataContainer($values)) {
+      if ($values->getType == 'array') {
+        return $values;
+      }
+      // Convert the container of single type into a container of array.
+      return new Core\DataContainer([$values], 'array');
+    }
+
+    // Convert single value into an array container.
     if (!is_array($values)) {
-      return array($values);
+      return new Core\DataContainer([$values], 'array');
     }
 
-    if (!Core\Utilities::is_assoc($values)) {
-      return $values;
-    }
-
-    $result = array();
+    // We have an array, keys can be computed to allow dynamic associative arrays.
+    $result = [];
     foreach ($values as $key => $value) {
-      $data = $this->isDataContainer($value) ? $value->getData() : $value;
-      $result[] = array($key => $data);
+      $key = $this->val($key);
+      $value = $this->val($value);
+      $result[$key] = $value;
     }
-
     return new Core\DataContainer($result, 'array');
   }
 }
