a9fb2959df0896f9bfba06a7a518c8ee5276d5e1
John <3242482-john89@users.noreply.gitlab.com>
2019-09-30 18:06:03 +1000

    Application create/update.

--- includes/Processor/Application.php
+++ includes/Processor/Application.php
@@ -8,66 +8,91 @@
 
 namespace Gaterdata\Processor;
 use Gaterdata\Core;
+use Gaterdata\Core\Config;
+use Gaterdata\Core\Debug;
 use Gaterdata\Db;
 
 class Application extends Core\ProcessorEntity
 {
-  protected $details = array(
+  protected $details = [
     'name' => 'Application',
-    'machineName' => 'datagatorApplication',
-    'description' => 'CRUD operations for Datagator applications.',
+    'machineName' => 'application',
+    'description' => 'CRUD operations for applications.',
     'menu' => 'Admin',
     'application' => 'Admin',
-    'input' => array(
-      'applicationName' => array(
+    'input' => [
+      'accountName' => [
+        'description' => 'The name of the account the application is assigned to.',
+        'cardinality' => [1, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['string'],
+        'limitValues' => [],
+        'default' => ''
+      ],
+      'applicationName' => [
         'description' => 'The name of the application.',
-        'cardinality' => array(1, 1),
-        'literalAllowed' => true,
-        'limitFunctions' => array(),
-        'limitTypes' => array('string'),
-        'limitValues' => array(),
+        'cardinality' => [1, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['string'],
+        'limitValues' => [],
         'default' => ''
-      ),
-      'accountName' => array(
-        'description' => 'The name of the account.',
-        'cardinality' => array(0, 1),
-        'literalAllowed' => true,
-        'limitFunctions' => array(),
-        'limitTypes' => array('string'),
-        'limitValues' => array(),
+      ],
+      'newAccountName' => [
+        'description' => 'The new name of the account (only used if updating an application).',
+        'cardinality' => [0, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['string'],
+        'limitValues' => [],
         'default' => ''
-      ),
-    ),
-  );
+      ],
+      'newApplicationName' => [
+        'description' => 'The new name of the application (only used if updating an application).',
+        'cardinality' => [0, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['string'],
+        'limitValues' => [],
+        'default' => ''
+      ],
+    ],
+  ];
 
   public function process()
   {
-    Core\Debug::variable($this->meta, 'Processor DatagatorApplication', 4);
+    Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
 
-    $applicationName = $this->val('applicationName');
-    $method = $this->request->method;
-    $db = $this->getDb();
+    $accountMapper = new Db\AccountMapper($this->db);
+    $applicationMapper = new Db\ApplicationMapper($this->db);
 
-    $accountId = '';
-    if (!empty($this->meta->accountName)) {
-      $accountName = $this->val('accountName');
-      $accountMapper = new Db\AccountMapper($db);
-      $account = $accountMapper->findByName($accountName);
-      $accountId = $account->getAccId();
-      if (empty($accountId)) {
-        throw new Core\ApiException("No such account exists: $accountName", 1, $this->id);
-      }
+    $accountName = $this->val('accountName', TRUE);
+    $applicationName = $this->val('applicationName', TRUE);
+    $method = $this->request->getMethod();
+
+    $account = $accountMapper->findByName($accountName);
+    $accountId = $account->getAccId();
+    if (empty($accountId)) {
+      throw new Core\ApiException("No such account exists: $accountName", 6, $this->id);
     }
 
-    $applicationMapper = new Db\ApplicationMapper($db);
+    $application = $applicationMapper->findByAccidAppname($accountId, $applicationName);
 
     switch ($method) {
 
       case 'post':
-        $application = $applicationMapper->findByName($applicationName);
+        $newAccountName = $this->val('newAccountName', TRUE);
+        $newApplicationName = $this->val('newApplicationName', TRUE);
+        if (!empty($newAccountName)) {
+          $account = $accountMapper->findByName($newAccountName);
+          if (empty($accountId = $account->getAccId())) {
+            throw new Core\ApiException("No such new account exists: $newAccountName", 6, $this->id);
+          }
+        }
         $application->setAccId($accountId);
-        $application->setName($applicationName);
-        Core\Debug::variable($application->debug());
+        $application->setName(!empty($newApplicationName) ? $newApplicationName : $applicationName);
+        Debug::variable($account->dump());
         return $applicationMapper->save($application);
         break;
 
