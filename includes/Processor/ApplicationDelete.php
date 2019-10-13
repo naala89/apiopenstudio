60bf2ba64ca1d866008ba4fefaa59a2503087c1c
John <3242482-john89@users.noreply.gitlab.com>
2019-10-13 11:43:08 +1100

    Application delete.

---
+++ includes/Processor/ApplicationDelete.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Delete an application.
+ */
+
+namespace Gaterdata\Processor;
+use Gaterdata\Core;
+use Gaterdata\Core\ApiException;
+use Gaterdata\Db;
+use Gaterdata\Db\UserRoleMapper;
+
+class ApplicationDelete extends Core\ProcessorEntity
+{
+  protected $details = [
+    'name' => 'Application delete',
+    'machineName' => 'application_delete',
+    'description' => 'Delete an application.',
+    'menu' => 'Admin',
+    'input' => [
+      'applicationId' => [
+        'description' => 'The appication ID of the application.',
+        'cardinality' => [1, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['integer'],
+        'limitValues' => [],
+        'default' => ''
+      ],
+    ],
+  ];
+
+  public function process()
+  {
+    Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
+
+    $appid = $this->val('applicationId', TRUE);
+
+    $applicationMapper = new Db\ApplicationMapper($this->db);
+    $resourceMapper = new Db\ResourceMapper($this->db);
+    $userRoleMapper = new Db\UserRoleMapper($this->db);
+
+    $application = $applicationMapper->findByAppid($appid);
+    if (empty($application->getAppid())) {
+      throw new ApiException("Delete application, no such appid: $appid", 6, $this->id, 417);
+    }
+    $resources = $resourceMapper->findByAppId($appid);
+    if (!empty($resources)) {
+      throw new ApiException("Delete application, resources are assigned to this application: $appid", 6, $this->id, 417);
+    }
+    $userRoles = $userRoleMapper->findByAppid($appid);
+    if (!empty($userRoles)) {
+      throw new ApiException("Delete application, users are assigned to this application: $appid", 6, $this->id, 417);
+    }
+
+    return $applicationMapper->delete($application);
+  }
+}
