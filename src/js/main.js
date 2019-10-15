039f6d8963c5d6ef44cf6462f1b9108bad18d12c
John <3242482-john89@users.noreply.gitlab.com>
2019-10-15 18:35:27 +1100

    App edit/delete, margin between action buttons.

--- src/js/main.js
+++ src/js/main.js
@@ -28,9 +28,9 @@ $(document).ready(function() {
   // Edit application modal.
   $('.modal-app-edit-trigger').click(function() {
     var modal = $('#modal-app-edit');
-    var accid = $(this).attr('acc-id');
-    var appid = $(this).attr('app-id');
-    var name = $(this).attr('app-name');
+    var accid = $(this).attr('accid');
+    var appid = $(this).attr('appid');
+    var name = $(this).attr('name');
     var select_accid = modal.find('select[name="edit-app-accid"]');
 
     select_accid.find('option[value="' + accid + '"]').prop('selected', true);
@@ -43,8 +43,8 @@ $(document).ready(function() {
   // Delete application modal.
   $('.modal-app-delete-trigger').click(function() {
     var modal = $('#modal-app-delete');
-    var appid = $(this).attr('app-id');
-    var name = $(this).attr('app-name');
+    var appid = $(this).attr('appid');
+    var name = $(this).attr('name');
     modal.find('input[name="delete-app-appid"]').val(appid);
     modal.find('#delete-app-name').html(name);
     modal.modal('open');
