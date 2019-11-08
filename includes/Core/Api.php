1f499fd407873ece980f4cc11584386747a8689b
John.Avery <john.avery@adg.com.au>
2019-11-08 17:20:07 +1100

    Uri parts more complex first.

--- includes/Core/Api.php
+++ includes/Core/Api.php
@@ -200,7 +200,7 @@ class Api
             'resource' => $result,
           ];
         }
-        array_shift($uriParts);
+        array_pop($uriParts);
       }
       throw new ApiException('invalid request', 3, -1, 404);
     }
