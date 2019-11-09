379eb33c8ab49155e58c9236c578168855ed8a70
John <3242482-john89@users.noreply.gitlab.com>
2019-11-10 10:52:01 +1100

    User update taking user details from body.

--- includes/Processor/VarBody.php
+++ includes/Processor/VarBody.php
@@ -14,28 +14,27 @@
 
 namespace Gaterdata\Processor;
 use Gaterdata\Core;
+use Gaterdata\Core\ApiException;
 use Gaterdata\Core\Debug;
 
 class VarBody extends VarMixed
 {
-  protected $details = array(
+  protected $details = [
     'name' => 'Var (Body)',
     'machineName' => 'var_body',
     'description' => 'Fetch the entire body of a post into a variable.',
     'menu' => 'Primitive',
     'application' => 'Common',
-    'input' => array(),
-  );
+    'input' => [],
+  ];
 
   /**
-   * @return mixed
-   * @throws \Gaterdata\Core\ApiException
-   * @throws \Gaterdata\Processor\ApiException
+   * {inheritDocs}
    */
   public function process()
   {
     Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
     $body = file_get_contents('php://input');
-    return $body;
+    return new Core\DataContainer($body, 'text');
   }
 }
