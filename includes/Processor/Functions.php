b1e71d204acddf2af9ad0cc349a17c2a6a1ea3f1
John <3242482-john89@users.noreply.gitlab.com>
2019-11-29 19:34:09 +1100

    Renamed ProcessorsAll to Functions and fixed code.

---
+++ includes/Processor/Functions.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Fetch a list of all processors
+ */
+
+namespace Gaterdata\Processor;
+
+use Gaterdata\Core;
+use Gaterdata\Endpoint;
+use Gaterdata\Output;
+use Gaterdata\Security;
+use RecursiveIteratorIterator;
+use ReflectionClass;
+use RegexIterator;
+
+class Functions extends Core\ProcessorEntity
+{
+  /**
+   * {@inheritDoc}
+   */
+  protected $details = [
+    'name' => 'Functions',
+    'machineName' => 'functions',
+    'description' => 'Fetch data on a single or all Functions.',
+    'menu' => 'System',
+    'input' => [
+      'machine_name' => [
+        'description' => 'The resource machine_name or "all" for all functions.',
+        'cardinality' => [1, 1],
+        'literalAllowed' => TRUE,
+        'limitFunctions' => [],
+        'limitTypes' => ['string'],
+        'limitValues' => [],
+        'default' => '',
+      ],
+    ],
+  ];
+
+  /**
+   * @var array list of namespaces to fetch.
+   */
+  private $namespaces = [
+    'Endpoint',
+    'Output',
+    'Processor',
+    'Security',
+  ];
+
+  /**
+   * {@inheritDoc}
+   */
+  public function process()
+  {
+    Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
+
+    $machineName = $this->val('machine_name', TRUE);
+
+    $details = [];
+    foreach ($this->namespaces as $namespace) {
+      $classNames = $this->_getClassList($namespace);
+      foreach ($classNames as $className) {
+        $detail = $this->_getDetails($namespace, $className);
+        $details[$detail['machineName']] =  $detail;
+      }
+    }
+
+    if ($machineName == 'all') {
+      return $details;
+    }
+
+    if (!isset($details[$machineName])) {
+      throw new Core\ApiException("Invalid machine name: $machineName",6, $this->id, 401);
+    }
+    return $details[$machineName];
+  }
+
+  /**
+   * Get a list of classes from a directory.
+   *
+   * @param string $namespace
+   *   The subdirectory/namespace.
+   *
+   * @return array
+   *   The list of class names.
+   */
+  private function _getClassList($namespace)
+  {
+    $iterator = new RecursiveIteratorIterator(new \RecursiveDirectoryIterator(__DIR__ . '/../' . $namespace));
+    $objects = new RegexIterator($iterator, '/[a-z0-9]+\.php/i', \RecursiveRegexIterator::GET_MATCH);
+    $result = [];
+    foreach($objects as $name => $object) {
+      preg_match('/([a-zA-Z0-9]+)\.php$/i', $name, $className);
+      $result[] = $className[1];
+    }
+    return $result;
+  }
+
+  /**
+   * Return the default details attributed from a class.
+   *
+   * @param string $namespace
+   *   The namespace that the class belongs to.
+   * @param string $className
+   *   The classname.
+   *
+   * @return array
+   *   The details array.
+   *
+   * @throws \ReflectionException
+   */
+  private function _getDetails($namespace, $className)
+  {
+    $reflector = new ReflectionClass("\\Gaterdata\\$namespace\\$className");
+    $properties = $reflector->getDefaultProperties();
+
+    return !$reflector->isAbstract() && !empty($properties['details']) ? $properties['details'] : [];
+  }
+}
