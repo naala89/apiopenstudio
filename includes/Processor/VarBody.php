5995da11ddde4fa68cb555536909d63beaea3fc5
John <3242482-john89@users.noreply.gitlab.com>
2019-12-20 07:24:57 +1100

    VarBody to create datacontainers from mixed input.

--- includes/Processor/VarBody.php
+++ includes/Processor/VarBody.php
@@ -17,6 +17,7 @@ namespace Gaterdata\Processor;
 use Gaterdata\Core;
 use Gaterdata\Core\ApiException;
 use Gaterdata\Core\Debug;
+use phpDocumentor\Reflection\Types\Integer;
 
 class VarBody extends VarMixed
 {
@@ -26,9 +27,43 @@ class VarBody extends VarMixed
     protected $details = [
         'name' => 'Var (Body)',
         'machineName' => 'var_body',
-        'description' => 'Fetch the entire body of a post into a variable.',
+        'description' => 'Fetch the entire body of a post.',
         'menu' => 'Primitive',
-        'input' => [],
+        'input' => [
+            'type' => [
+                // phpcs:ignore
+                'description' => 'The expected data type in the body. A value of "auto" will auto detect the content type, but this can only detect boolean, integer, float, valid json, valid xml, valid html or text.',
+                'cardinality' => [0, 1],
+                'literalAllowed' => true,
+                'limitFunctions' => [],
+                'limitTypes' => [],
+                'limitValues' => [
+                    'boolean',
+                    'integer',
+                    'float',
+                    'json',
+                    'html',
+                    'xml',
+                    'text',
+                    'image',
+                    'file',
+                    'auto',
+                ],
+                'default' => 'auto',
+            ],
+            'nullable' => [
+                'description' => 'Thow an error if the body is empty.',
+                'cardinality' => [0, 1],
+                'literalAllowed' => true,
+                'limitFunctions' => [],
+                'limitTypes' => [],
+                'limitValues' => [
+                    true,
+                    false,
+                ],
+                'default' => 'true',
+            ],
+        ],
     ];
 
     /**
@@ -37,7 +72,37 @@ class VarBody extends VarMixed
     public function process()
     {
         Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
-        $body = file_get_contents('php://input');
-        return new Core\DataContainer($body, 'text');
+
+        $type = $this->val('type', true);
+        $nullable = $this->val('nullable', true);
+        $data = file_get_contents('php://input');
+
+        if (!$nullable && empty($data)) {
+            throw new ApiException("Body is empty", 6, $this->id);
+        }
+        if ($type != 'auto') {
+            return new Core\DataContainer($data, $type);
+        }
+
+        $type = $this->detectType($data);
+        switch ($type) {
+            case 'boolean':
+                return new Core\DataContainer(filter_var($data, FILTER_VALIDATE_BOOLEAN), $type);
+                break;
+            case 'integer':
+                return new Core\DataContainer(intval($data), $type);
+                break;
+            case 'float':
+                return new Core\DataContainer(floatval($data), $type);
+                break;
+            case 'json':
+            case 'html':
+            case 'xml':
+            case 'text':
+            case 'image':
+            case 'file':
+                return new Core\DataContainer($data, $type);
+                break;
+        }
     }
 }
