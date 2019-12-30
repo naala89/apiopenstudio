e0d2d752ef92424d0ec2c716688b16fa1c386c26
John.Avery <john.avery@adg.com.au>
2019-12-31 10:36:39 +1100

    Roles listing page.

--- includes/Admin/Controllers/CtrlRole.php
+++ includes/Admin/Controllers/CtrlRole.php
@@ -55,187 +55,44 @@ class CtrlRole extends CtrlBase
         $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
         $this->getAccessRights($response, $uid);
         if (!$this->checkAccess()) {
-            $this->flash->addMessage('error', 'View resources: access denied');
+            $this->flash->addMessage('error', 'View roles: access denied');
             return $response->withStatus(302)->withHeader('Location', '/');
         }
 
         $menu = $this->getMenus();
-//        $params = $request->getQueryParams();
+        $allParams = $request->getParams();
+
+        $query = [];
+        if (!empty($allParams['keyword'])) {
+            $query['keyword'] = $allParams['keyword'];
+        }
+        if (!empty($allParams['direction'])) {
+            $query['order_by'] = 'name';
+            $query['direction'] = $allParams['direction'];
+        }
+//        var_dump($query);die;
 
         try {
-            $result = $client->request('GET', 'user/role', [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
+            $result = $this->apiCall('get', 'role/all',
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer " . $_SESSION['token'],
+                        'Accept' => 'application/json',
+                    ],
+                    'query' => $query,
                 ],
-                'query' => $params,
-            ]);
-            $userRoles = (array)json_decode($result->getBody()->getContents());
-            $result = $client->request('GET', 'account/all', [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
-                ],
-            ]);
-            $accounts = (array)json_decode($result->getBody()->getContents());
-            $result = $client->request('GET', 'application', [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
-                ],
-            ]);
-            $applications = (array)json_decode($result->getBody()->getContents());
-            $result = $client->request('GET', 'user', [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
-                ],
-            ]);
-            $users = (array)json_decode($result->getBody()->getContents());
-        } catch (ClientException $e) {
-            $result = $e->getResponse();
-            $this->flash->addMessage('error', $this->getErrorMessage($e));
-            switch ($result->getStatusCode()) {
-                case 401:
-                    return $response->withStatus(302)->withHeader('Location', '/login');
-                    break;
-                default:
-                    return $this->view->render($response, 'user-roles.twig', [
-                        'menu' => $menu,
-                        'user_roles' => [],
-                        'messages' => $this->flash->getMessages(),
-                    ]);
-                    break;
-            }
+                $response
+            );
+            $roles = json_decode($result->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            $this->flash->addMessageNow($e->getMessage());
+            $roles = [];
         }
 
-        return $this->view->render($response, 'user-roles.twig', [
+        return $this->view->render($response, 'roles.twig', [
             'menu' => $menu,
-            'params' => $params,
-            'user_roles' => $userRoles,
-            'accounts' => $accounts,
-            'applications' => $applications,
-            'users' => $users,
-            'roles' => $this->allRoles,
+            'roles' => $roles,
             'messages' => $this->flash->getMessages(),
         ]);
-    }
-
-    /**
-     * Create a user role.
-     *
-     * @param Request $request
-     *   Request object.
-     * @param Response $response
-     *   Response object.
-     * @param array $args
-     *   Request args.
-     *
-     * @return ResponseInterface
-     *   Response.
-     *
-     * @throws GuzzleException
-     */
-    public function create(Request $request, Response $response, array $args)
-    {
-        // Validate access.
-        $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
-        $this->getAccessRights($response, $uid);
-        if (!$this->checkAccess()) {
-            $this->flash->addMessage('error', 'Access admin: access denied');
-            return $response->withStatus(302)->withHeader('Location', '/');
-        }
-
-        $allPostVars = $request->getParsedBody();
-        $domain = $this->settings['api']['url'];
-        $account = $this->settings['api']['core_account'];
-        $application = $this->settings['api']['core_application'];
-        $token = $_SESSION['token'];
-        $client = new Client(['base_uri' => "$domain/$account/$application/"]);
-
-        try {
-            $result = $client->request('POST', 'user/role', [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
-                ],
-                'form_params' => [
-                    'uid' => $allPostVars['uid'],
-                    'accid' => $allPostVars['accid'],
-                    'appid' => $allPostVars['appid'],
-                    'rid' => $allPostVars['rid'],
-                ],
-            ]);
-        } catch (ClientException $e) {
-            $result = $e->getResponse();
-            $this->flash->addMessage('error', $this->getErrorMessage($e));
-            switch ($result->getStatusCode()) {
-                case 401:
-                    return $response->withStatus(302)->withHeader('Location', '/login');
-                    break;
-                default:
-                    return $response->withStatus(302)->withHeader('Location', '/user/roles');
-                    break;
-            }
-        }
-
-        $this->flash->addMessage('info', 'User role created.');
-        return $response->withStatus(302)->withHeader('Location', '/user/roles');
-    }
-
-    /**
-     * Delete a user role.
-     *
-     * @param Request $request
-     *   Request object.
-     * @param Response $response
-     *   Response object.
-     * @param array $args
-     *   Request args.
-     *
-     * @return ResponseInterface
-     *   Response.
-     *
-     * @throws GuzzleException
-     */
-    public function delete(Request $request, Response $response, array $args)
-    {
-        // Validate access.
-        $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
-        $this->getAccessRights($response, $uid);
-        if (!$this->checkAccess()) {
-            $this->flash->addMessage('error', 'Access admin: access denied');
-            return $response->withStatus(302)->withHeader('Location', '/');
-        }
-
-        $allPostVars = $request->getParsedBody();
-        $urid = $allPostVars['urid'];
-        if (empty($urid)) {
-            $this->flash->addMessage('error', 'Cannot delete user role, user role unspecified.');
-            return $response->withStatus(302)->withHeader('Location', '/user/roles');
-        }
-
-        $domain = $this->settings['api']['url'];
-        $account = $this->settings['api']['core_account'];
-        $application = $this->settings['api']['core_application'];
-        $token = $_SESSION['token'];
-        $client = new Client(['base_uri' => "$domain/$account/$application/"]);
-
-        try {
-            $result = $client->request('DELETE', 'user/role/' . $urid, [
-                'headers' => [
-                    'Authorization' => "Bearer $token",
-                ],
-            ]);
-        } catch (ClientException $e) {
-            $result = $e->getResponse();
-            $this->flash->addMessage('error', $this->getErrorMessage($e));
-            switch ($result->getStatusCode()) {
-                case 401:
-                    return $response->withStatus(302)->withHeader('Location', '/login');
-                    break;
-                default:
-                    return $response->withStatus(302)->withHeader('Location', '/user/roles');
-                    break;
-            }
-        }
-
-        $this->flash->addMessage('info', 'User role deleted.');
-        return $response->withStatus(302)->withHeader('Location', '/user/roles');
     }
 }
