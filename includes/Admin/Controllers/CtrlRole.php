4873d2c09f0a7aeb8e071c6b0b3c2c64d0a4783d
John.Avery <john.avery@adg.com.au>
2019-12-31 14:13:25 +1100

    Role create, edit, delete.

--- includes/Admin/Controllers/CtrlRole.php
+++ includes/Admin/Controllers/CtrlRole.php
@@ -2,12 +2,8 @@
 
 namespace Gaterdata\Admin\Controllers;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\TransferException;
 use Psr\Http\Message\ResponseInterface;
-use Slim\Views\Twig;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Gaterdata\Admin\Account;
@@ -70,7 +66,6 @@ class CtrlRole extends CtrlBase
             $query['order_by'] = 'name';
             $query['direction'] = $allParams['direction'];
         }
-//        var_dump($query);die;
 
         try {
             $result = $this->apiCall('get', 'role/all',
@@ -89,10 +84,184 @@ class CtrlRole extends CtrlBase
             $roles = [];
         }
 
+        // Pagination.
+        $page = isset($allParams['page']) ? $allParams['page'] : 1;
+        $pages = ceil(count($roles) / $this->settings['admin']['pagination_step']);
+        $roles = array_slice(
+            $roles,
+            ($page - 1) * $this->settings['admin']['pagination_step'],
+            $this->settings['admin']['pagination_step'],
+            true
+        );
+
         return $this->view->render($response, 'roles.twig', [
             'menu' => $menu,
             'roles' => $roles,
+            'page' => $page,
+            'pages' => $pages,
             'messages' => $this->flash->getMessages(),
         ]);
+    }
+
+    /**
+     * Create a role
+     *
+     * @param Request $request
+     *   Request object.
+     * @param Response $response
+     *   Response object.
+     * @param array $args
+     *   Request args.
+     *
+     * @return ResponseInterface
+     *   Response.
+     *
+     * @throws GuzzleException
+     */
+    public function create(Request $request, Response $response, array $args)
+    {
+        // Validate access.
+        $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
+        $this->getAccessRights($response, $uid);
+        if (!$this->checkAccess()) {
+            $this->flash->addMessage('error', 'View roles: access denied');
+            return $response->withStatus(302)->withHeader('Location', '/');
+        }
+
+        $menu = $this->getMenus();
+        $allParams = $request->getParams();
+
+        $name = !empty($allParams['name']) ? $allParams['name'] : '';
+
+        try {
+            $result = $this->apiCall('post', "role",
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer " . $_SESSION['token'],
+                        'Accept' => 'application/json',
+                    ],
+                    'form_params' => ['name' => $name],
+                ],
+                $response
+            );
+            $success = json_decode($result->getBody()->getContents(), true);
+            if ($success == 'true') {
+                $this->flash->addMessageNow('info', 'Role successfully created.');
+            } else {
+                $this->flash->addMessageNow('error', 'Role creation failed, please check the logs.');
+            }
+        } catch (\Exception $e) {
+            $this->flash->addMessageNow('error', $e->getMessage());
+        }
+
+        return $this->index($request, $response, $args);
+    }
+
+    /**
+     * Update a role
+     *
+     * @param Request $request
+     *   Request object.
+     * @param Response $response
+     *   Response object.
+     * @param array $args
+     *   Request args.
+     *
+     * @return ResponseInterface
+     *   Response.
+     *
+     * @throws GuzzleException
+     */
+    public function update(Request $request, Response $response, array $args)
+    {
+        // Validate access.
+        $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
+        $this->getAccessRights($response, $uid);
+        if (!$this->checkAccess()) {
+            $this->flash->addMessage('error', 'View roles: access denied');
+            return $response->withStatus(302)->withHeader('Location', '/');
+        }
+
+        $menu = $this->getMenus();
+        $allParams = $request->getParams();
+
+        $name = !empty($allParams['name']) ? $allParams['name'] : '';
+        $rid = !empty($allParams['rid']) ? $allParams['rid'] : '';
+
+        try {
+            $result = $this->apiCall('put', 'role',
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer " . $_SESSION['token'],
+                        'Accept' => 'application/json',
+                    ],
+                    'json' => ['rid' => $rid, 'name' => $name],
+                ],
+                $response
+            );
+            $success = json_decode($result->getBody()->getContents(), true);
+            if ($success == 'true') {
+                $this->flash->addMessageNow('info', 'Role successfully updated.');
+            } else {
+                $this->flash->addMessageNow('error', 'Role update failed, please check the logs.');
+            }
+        } catch (\Exception $e) {
+            $this->flash->addMessageNow('error', $e->getMessage());
+        }
+
+        return $this->index($request, $response, $args);
+    }
+
+    /**
+     * Delete a role
+     *
+     * @param Request $request
+     *   Request object.
+     * @param Response $response
+     *   Response object.
+     * @param array $args
+     *   Request args.
+     *
+     * @return ResponseInterface
+     *   Response.
+     *
+     * @throws GuzzleException
+     */
+    public function delete(Request $request, Response $response, array $args)
+    {
+        // Validate access.
+        $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : '';
+        $this->getAccessRights($response, $uid);
+        if (!$this->checkAccess()) {
+            $this->flash->addMessage('error', 'View roles: access denied');
+            return $response->withStatus(302)->withHeader('Location', '/');
+        }
+
+        $menu = $this->getMenus();
+        $allParams = $request->getParams();
+
+        $rid = !empty($allParams['rid']) ? $allParams['rid'] : '';
+
+        try {
+            $result = $this->apiCall('delete', "role/$rid",
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer " . $_SESSION['token'],
+                        'Accept' => 'application/json',
+                    ],
+                ],
+                $response
+            );
+            $success = json_decode($result->getBody()->getContents(), true);
+            if ($success == 'true') {
+                $this->flash->addMessageNow('info', 'Role successfully deleted.');
+            } else {
+                $this->flash->addMessageNow('error', 'Role deletion failed, please check the logs.');
+            }
+        } catch (\Exception $e) {
+            $this->flash->addMessageNow('error', $e->getMessage());
+        }
+
+        return $this->index($request, $response, $args);
     }
 }
---
+++ includes/Processor/RoleUpdate.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Update a role.
+ */
+
+namespace Gaterdata\Processor;
+
+use Gaterdata\Core;
+use Gaterdata\Db\RoleMapper;
+
+class RoleUpdate extends Core\ProcessorEntity
+{
+    /**
+     * @var RoleMapper
+     */
+    private $roleMapper;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $details = [
+        'name' => 'Role update',
+        'machineName' => 'role_update',
+        'description' => 'Update a role.',
+        'menu' => 'Admin',
+        'input' => [
+            'rid' => [
+                'description' => 'The ID of the role.',
+                'cardinality' => [1, 1],
+                'literalAllowed' => true,
+                'limitFunctions' => [],
+                'limitTypes' => ['integer'],
+                'limitValues' => [],
+                'default' => 0,
+            ],
+            'name' => [
+                'description' => 'The new name of the role.',
+                'cardinality' => [1, 1],
+                'literalAllowed' => true,
+                'limitFunctions' => [],
+                'limitTypes' => ['integer', 'text'],
+                'limitValues' => [],
+                'default' => '',
+            ]
+        ],
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($meta, &$request, $db)
+    {
+        parent::__construct($meta, $request, $db);
+        $this->roleMapper = new RoleMapper($db);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process()
+    {
+        Core\Debug::variable($this->meta, 'Processor ' . $this->details()['machineName'], 2);
+
+        $rid = $this->val('rid', true);
+        $name = $this->val('name', true);
+
+        if ($rid < 6) {
+            throw new Core\ApiException("Cannot update core roles.", 7, $this->id);
+        }
+
+        $role = $this->roleMapper->findByName($name);
+        if (!empty($role->getRid())) {
+            throw new Core\ApiException("A role with the name '$name' already exists", 7, $this->id);
+        }
+        $role = $this->roleMapper->findByRid($rid);
+        if (empty($role->getRid())) {
+            throw new Core\ApiException("A role with RID: $rid does not exist", 7, $this->id);
+        }
+
+        $role->setName($name);
+
+        return $this->roleMapper->save($role);
+    }
+}
