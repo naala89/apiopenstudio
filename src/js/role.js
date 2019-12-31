1ff9a057fe3c4344eac2b247389cad307e23df8e
John <3242482-john89@users.noreply.gitlab.com>
2019-12-31 14:12:43 +1100

    Separate page js files.

---
+++ src/js/role.js
@@ -0,0 +1,26 @@
+/**
+ * JS for Roles page.
+ */
+$(document).ready(function () {
+    $('.modal-role-create-trigger').click(function () {
+        var modal = $('#modal-role-create');
+        modal.modal('open');
+    });
+
+    $('.modal-role-edit-trigger').click(function () {
+        var modal = $('#modal-role-update'),
+            self = $(this);
+        modal.find('input[name="rid"]').val(self.attr('rid'));
+        modal.find('input[name="name"]').val(self.attr('role-name'));
+        M.updateTextFields();
+        modal.modal('open');
+    });
+
+    $('.modal-role-delete-trigger').click(function () {
+        var modal = $('#modal-role-delete'),
+            self = $(this);
+        modal.find('input[name="rid"]').val(self.attr('rid'));
+        modal.find('.role-name').html(self.attr('role-name'));
+        modal.modal('open');
+    });
+});
---
+++ src/js/user.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    /**
+     * Delete user modal.
+     */
+    $('.modal-user-delete-trigger').click(function () {
+        var self = $(this);
+        var modal = $('#modal-user-delete');
+        modal.find('#user-name').html(self.attr('delete-user-username'));
+        modal.find('a#delete-user').attr('href', '/user/delete/' + self.attr('delete-user-uid'));
+        modal.modal('open');
+    });
+});
