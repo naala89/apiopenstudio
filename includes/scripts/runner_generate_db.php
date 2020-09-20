fe7bc5bb9ff78bfa7176dc5759e5c3c92e1059df
John <3242482-john89@users.noreply.gitlab.com>
2020-09-20 12:04:53 +1000

    Generate the DB script first pass.

--- includes/scripts/runner_generate_db.php
+++ includes/scripts/runner_generate_db.php
@@ -32,5 +32,95 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
+// Create the database, user and permissions.
+$sql = 'CREATE DATABASE ' . getenv('MYSQL_DATABASE') . 'IF NOT EXISTS';
+$conn->query($sql);
+$sql = 'CREATE USER IF NOT EXISTS "' . getenv('MYSQL_USERNAME') . '"@"';
+$sql .= getenv('MYSQL_HOST') . '" IDENTIFIED BY "' . getenv('MYSQL_PASSWORD') . '"';
+$conn->query($sql);
+$sql = 'GRANT ALL PRIVILEGES ON * . * TO "' . getenv('MYSQL_USERNAME');
+$sql .= '"@"' . getenv('MYSQL_HOST') . '"';
+$conn->query($sql);
+$sql = 'FLUSH PRIVILEGES';
+$conn->query($sql);
+
 $yaml = file_get_contents(dirname(dirname(__DIR__)) . '/includes/Db/dbDefinition.yaml');
 $definition = \Spyc::YAMLLoadString($yaml);
+
+// Parse the DB  table definition array.
+foreach ($definition as $table => $tableData) {
+    $sqlPrimary = '';
+    $sqlColumns = [];
+    foreach ($tableData['columns'] as $column => $columnData) {
+        // Column definitions.
+        $sqlColumn = "`$column` ";
+        if (!isset($columnData['type'])) {
+            echo "CREATE TABLE `$table` fail!";
+            echo 'Type missing in the metadata.';
+            exit;
+        }
+        $sqlColumn .= ' ' . $columnData['type'];
+        $sqlColumn .= isset($columnData['notnull']) && $columnData['notnull'] ? ' NOT null' : '';
+        $sqlColumn .= isset($columnData['default']) ? (' DEFAULT ' . $columnData['default']) : '';
+        $sqlColumn .= isset($columnData['autoincrement']) ? ' AUTO_INCREMENT' : '';
+        $sqlColumn .= isset($columnData['primary']) ? ' PRIMARY KEY' : '';
+        $sqlColumn .= isset($columnData['comment']) ? (" COMMENT '" . $columnData['comment'] . "'") : '';
+        $sqlColumns[] = $sqlColumn;
+    }
+    $sqlCreate = "CREATE TABLE IF NOT EXISTS `$table` (" . implode(', ', $sqlColumns) . ');';
+    if (empty($conn->query($sqlCreate))) {
+        // Stop if table create fails.
+
+        echo "$sqlCreate";
+        echo "CREATE TABLE `$table` fail!";
+        exit;
+    }
+
+    // Add data if required.
+    if (isset($tableData['data'])) {
+        foreach ($tableData['data'] as $row) {
+            $keys = [];
+            $values = [];
+            foreach ($row as $key => $value) {
+                $keys[] = "`$key`";
+                $values[] = is_string($value) ? "\"$value\"" : $value;
+            }
+            $sqlRow = "INSERT INTO `$table` (" . implode(', ', $keys) . ')';
+            $sqlRow .= 'VALUES (' . implode(', ', $values) . ');';
+            if (empty($conn->query($sqlRow))) {
+                echo "INSERT into `$table` fail!";
+                exit;
+            }
+        }
+    }
+}
+
+// Add resource data from the resources directory
+$dir = '../../includes/resources';
+$filenames = scandir($dir);
+foreach ($filenames as $filename) {
+    if (pathinfo($filename, PATHINFO_EXTENSION) != 'yaml') {
+        continue;
+    }
+    $yaml = \Spyc::YAMLLoadString(file_get_contents("$dir/$filename"));
+    $name = $yaml['name'];
+    $description = $yaml['description'];
+    $uri = $yaml['uri'];
+    $method = $yaml['method'];
+    $appid = $yaml['appid'];
+    $ttl = $yaml['ttl'];
+    $meta = [];
+    if (!empty($yaml['security'])) {
+        $meta[] = '"security": ' . json_encode($yaml['security']);
+    }
+    if (!empty($yaml['process'])) {
+        $meta[] = '"process": ' . json_encode($yaml['process']);
+    }
+    $meta = '{' . implode(', ', $meta) . '}';
+    $sqlRow = 'INSERT INTO resource (`appid`, `name`, `description`, `method`, `uri`, `meta`, `ttl`)';
+    $sqlRow .= "VALUES ($appid, '$name', '$description', '$method', '$uri', '$meta', $ttl)";
+    if (empty($conn->query($sqlRow))) {
+        echo "INSERT $name into `resource` fail!";
+        exit;
+    }
+}
