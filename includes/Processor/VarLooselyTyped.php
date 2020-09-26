793d3114eda07e330c594fb003882f27c75d4945
John <3242482-john89@users.noreply.gitlab.com>
2020-09-26 10:15:06 +1000

    Refactor VarMixed into VarLooselyTyped.

---
+++ includes/Processor/VarLooselyTyped.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Class VarLooselyTyped.
+ *
+ * @package    Gaterdata
+ * @subpackage Processor
+ * @author     john89 (https://gitlab.com/john89)
+ * @copyright  2020-2030 GaterData
+ * @license    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *             If a copy of the MPL was not distributed with this file,
+ *             You can obtain one at https://mozilla.org/MPL/2.0/.
+ * @link       https://gaterdata.com
+ */
+
+namespace Gaterdata\Processor;
+
+use Gaterdata\Core;
+
+/**
+ * Class VarLooselyTyped
+ *
+ * Processor class to define a loosely typed variable.
+ */
+class VarLooselyTyped extends Core\ProcessorEntity
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @var array Details of the processor.
+     */
+    protected $details = [
+        'name' => 'Var (loosely typed)',
+        'machineName' => 'var_loosely_typed',
+        'description' => 'A loosely typed variable (i.e. not strictly typed).',
+        'menu' => 'Primitive',
+        'input' => [
+            'value' => [
+                'description' => 'The value of the variable.',
+                'cardinality' => [1, 1],
+                'literalAllowed' => true,
+                'limitFunctions' => [],
+                'limitTypes' => [],
+                'limitValues' => [],
+                'default' => '',
+            ],
+        ],
+    ];
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return Core\DataContainer Result of the processor.
+     *
+     * @throws Core\ApiException Exception if invalid result.
+     */
+    public function process()
+    {
+        $this->logger->info('Processor: ' . $this->details()['machineName']);
+
+        $result = $this->val('value');
+        $result->setType('text');
+
+        return $result;
+    }
+}
