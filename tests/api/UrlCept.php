d574d5f0f50f10c5af7ab248970982e62ea29b8d
John <3242482-john89@users.noreply.gitlab.com>
2020-09-28 08:58:53 +1000

    Test for SourceNotAltered.

--- tests/api/UrlCept.php
+++ tests/api/UrlCept.php
@@ -1,21 +1,31 @@
 <?php
 $I = new ApiTester($scenario);
 $I->performLogin();
-$I->setYamlFilename('url.yaml');
+$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
+$I->createResourceFromYaml('url.yaml');
+$I->deleteHeader('Authorization');
+
+$uri = $I->getMyBaseUri() . '/url';
 
 $I->wantTo('populate a Url with correct inputs (no auth) and see the result.');
-$I->createResourceFromYaml();
-$I->callResourceFromYaml([
-  'method' => 'get',
-  'url' => 'jsonplaceholder.typicode.com/posts/1',
-  'sourceType' => 'json',
-  'reportError' => true,
-  'connectTimeout' => 10,
-  'timeout' => 30
+$I->sendGet($uri, ['token' => $I->getMyStoredToken(),
+    'method' => 'get',
+    'url' => 'jsonplaceholder.typicode.com/posts/1',
+    'source_type' => 'json',
+    'report_error' => true,
+    'connect_timeout' => 10,
+    'timeout' => 30
 ]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 // phpcs:ignore
-$I->seeResponseContains('{"userId": 1, "id": 1, "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}');
+$I->seeResponseContainsJson([
+    "userId" => 1,
+    "id" => 1,
+    "title" => "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body" => "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+]);
 
-$I->tearDownTestFromYaml();
+$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
+$I->tearDownTestFromYaml('url.yaml');
+$I->deleteHeader('Authorization');
