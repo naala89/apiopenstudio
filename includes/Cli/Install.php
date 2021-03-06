94d61711e848623d66d377da6f31d88ec3b41663
John <noreply@apiopenstudio.com>
2021-03-06 11:11:19 +1100

    #85 Updated runner_generate_db to use Install.php, updated install.php, fixed some found bugs in Install.php.

--- includes/Cli/Install.php
+++ includes/Cli/Install.php
@@ -84,23 +84,27 @@ class Install extends Script
             exit;
         }
 
-        // DB link.
-        $dsnOptionsArr = [];
-        foreach ($this->config->__get(['db', 'options']) as $k => $v) {
-            $dsnOptionsArr[] = "$k=$v";
-        }
-        $dsnOptions = count($dsnOptionsArr) > 0 ? ('?' . implode('&', $dsnOptionsArr)) : '';
-        $dsn = $this->config->__get(['db', 'driver']) . '://'
-            . 'root' . ':'
-            . $this->config->__get(['db', 'root_password']) . '@'
-            . $this->config->__get(['db', 'host']) . '/'
-            . $this->config->__get(['db', 'database']) . $dsnOptions;
-        if (!$this->db = \ADONewConnection($dsn)) {
-            echo "Error: DB connection failed, please check your settings.yml file.\n";
-            exit;
+        $createDb = '';
+        while (!is_bool($createDb)) {
+            $prompt = "Recreate or create new database, user and user permissions (you must have the root_password in your settings.yml file) (Y/n)?";
+            $response = strtolower($this->readlineTerminal($prompt));
+            $createDb = $response === 'y' || empty($response) ? true : $createDb;
+            $createDb = $response === 'n' ? false : $createDb;
         }
 
-        $this->createDb();
+        if ($createDb) {
+            $this->createLink(null, null, '', 'root', $this->config->__get(['db', 'root_password']));
+            echo "\n";
+            $this->dropDatabase();
+            echo "\n";
+            $this->dropUser($this->config->__get(['db', 'username']));
+            echo "\n";
+            $this->createDatabase();
+            echo "\n";
+            $this->createUser();
+            echo "\n";
+        }
+        $this->useDatabase();
         echo "\n";
         $this->createTables();
         echo "\n";
@@ -111,52 +115,259 @@ class Install extends Script
     }
 
     /**
-     * Create the Database.
+     * Connect to the database.
+     *
+     * @param string|null $driver
+     *   Database driver.
+     * @param string|null $host
+     *   Database host.
+     * @param string|null $database
+     *   Database name.
+     * @param string|null $username
+     *   Database username.
+     * @param string|null $password
+     *   Database password.
      */
-    protected function createDb()
+    public function createLink(string $driver = null, string $host = null, string $database = null, string $username = null, string $password = null)
     {
-        // Create the database, user and permissions.
-        echo "Creating the Database..\n";
+        echo "Creating a connection the the database host...\n";
 
         try {
-            $sql = 'CREATE DATABASE ' . $this->config->__get(['db', 'database']) . 'IF NOT EXISTS';
-            $this->db->execute($sql);
-            $sql = 'CREATE USER IF NOT EXISTS "' . $this->config->__get(['db', 'username']) . '"@"';
-            $sql .= $this->config->__get(['db', 'host'])
-                . '" IDENTIFIED BY "'
-                . $this->config->__get(['db', 'password'])
-                . '"';
-            $this->db->execute($sql);
-            $sql = 'GRANT ALL PRIVILEGES ON * . * TO "' . $this->config->__get(['db', 'username']);
-            $sql .= '"@"' . $this->config->__get(['db', 'host']) . '"';
-            $this->db->execute($sql);
-            $sql = 'FLUSH PRIVILEGES';
-            $this->db->execute($sql);
-
-            echo 'Database `' . $this->config->__get(['db', 'database']) . "` created successfully!\n";
+            $driver = $driver === null ? $this->config->__get(['db', 'driver']) : $driver;
+            $host = $host === null ? $this->config->__get(['db', 'host']) : $host;
+            $database = $database === null ? $this->config->__get(['db', 'database']) : $database;
+            $username = $username === null ? $this->config->__get(['db', 'username']) : $username;
+            $password = $password === null ? $this->config->__get(['db', 'password']) : $password;
         } catch (ApiException $e) {
-            echo "Error creating the database:\n";
-            echo $e->getMessage() . "\n";
+            echo "Error: DB connection failed, please check your settings.yml file.\n";
+            echo $e->getMessage();
             exit;
         }
+
+        // DB link.
+        $this->db = adoNewConnection($driver);
+        if (empty($database)) {
+            if (!$this->db->connect($host, $username, $password)) {
+                echo "Error: DB connection failed.\n";
+                echo $this->db->errorMsg() . "\n";
+                exit;
+            }
+        }
+        else {
+            if (!$this->db->connect($host, $username, $password, $database)) {
+                echo "Error: DB connection failed.\n";
+                echo $this->db->errorMsg() . "\n";
+                exit;
+            }
+        }
+
+        echo "Connection successful!\n";
     }
 
     /**
-     * Create the tables anf populate them with initial Core data.
-     *
-     * @throws ApiException
+     * Close the DB link.
      */
-    protected function createTables()
+    public function closeLink()
+    {
+        echo "Closing connection the the database host...\n";
+
+        if (!$this->db->close()) {
+            echo "Error: Could not close the connection, please check the logs.\n";
+            exit;
+        }
+
+        echo "Disconnect successful!\n";
+    }
+
+    /**
+     * Create the database, user and permissions.
+     *
+     * @param string|null $database
+     *   Database name to create.
+     */
+    public function createDatabase(string $database = null)
+    {
+        try {
+            $database = $database === null ? $this->config->__get(['db', 'database']) : $database;
+        } catch (ApiException $e) {
+            echo "Error: Create `$database` database failed, please check your settings.yml file.\n";
+            echo $e->getMessage();
+            exit;
+        }
+
+        echo "Creating the `$database` Database...\n";
+
+        $sql = "CREATE DATABASE IF NOT EXISTS `$database`";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Create `$database` database failed, please check the logs.\n";
+            exit;
+        }
+
+        echo "Database `$database` created successfully!\n";
+    }
+
+    /**
+     * Drop a database.
+     *
+     * @param string|null $database
+     *   Database name to drop.
+     */
+    public function dropDatabase(string $database = null)
+    {
+        try {
+            $database = $database === null ? $this->config->__get(['db', 'database']) : $database;
+        } catch (ApiException $e) {
+            echo "Error: Drop `$database` database failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
+        }
+
+        echo "Dropping the `$database` Database...\n";
+
+        $sql = "DROP DATABASE IF EXISTS `$database`";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Drop database `$database` Failed, please check your logs.\n";
+            exit;
+        }
+
+        echo "Database `$database` dropped successfully!\n";
+    }
+
+    /**
+     * Use a database.
+     *
+     * @param string|null $database
+     *   Name of the database.
+     */
+    public function useDatabase(string $database = null)
+    {
+        try {
+            $database = $database === null ? $this->config->__get(['db', 'database']) : $database;
+        } catch (ApiException $e) {
+            echo "Error: use `$database` database failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
+        }
+
+        echo "Using the `$database` Database...\n";
+
+        $sql = "USE `$database`";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Use database `$database` Failed, please check your logs.\n";
+            exit;
+        }
+
+        echo "Using Database `$database` successful!\n";
+    }
+
+    /**
+     * Create the database, user and permissions.
+     *
+     * @param string|null $database
+     *   Database name.
+     * @param string|null $username
+     *   Database username to create.
+     * @param string|null $password
+     *   Database password to create.
+     */
+    public function createUser(string $database = null, string $username = null, string $password = null)
+    {
+        try {
+            $database = $database === null ? $this->config->__get(['db', 'database']) : $database;
+            $username = $username === null ? $this->config->__get(['db', 'username']) : $username;
+            $password = $password === null ? $this->config->__get(['db', 'password']) : $password;
+        } catch (ApiException $e) {
+            echo "Error: Create `$username` user failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
+        }
+
+        echo "Creating the `$username` user...\n";
+        $sql = "CREATE USER IF NOT EXISTS '$username'@'%' IDENTIFIED BY '$password'";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Create user `$username` failed, please check your logs.\n";
+            exit;
+        }
+        echo "Successfully created the `$username` user!\n";
+
+        echo "Granting `$database` database on `%` privileges to `$username`...\n";
+        $sql = "GRANT ALL PRIVILEGES ON $database.* TO '$username'@'%'";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Grant privileges failed, please check your logs.\n";
+            exit;
+        }
+        echo "Successfully granted privileges for `$username` user!\n";
+
+        $sql = 'FLUSH PRIVILEGES';
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Flush privileges failed, please check your logs.\n";
+            exit;
+        }
+
+        echo "User `$username` created successfully!\n";
+    }
+
+    /**
+     * Drop a suser with all their privileges.
+     *
+     * @param string|null $username
+     *   Username to drop.
+     */
+    public function dropUser(string $username = null) {
+        try {
+            $username = $username === null ? $this->config->__get(['db', 'username']) : $username;
+        } catch (ApiException $e) {
+            echo "Error: Drop user `$username` failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
+        }
+
+        echo "Dropping the `$username` user...\n";
+        $sql = "DROP USER IF EXISTS '$username'";
+        if (!$this->db->execute($sql)) {
+            echo "$sql\n";
+            echo "Error: Drop user `$username` failed, please check your logs.\n";
+            exit;
+        }
+        echo "Successfully dropped the `$username` user!\n";
+    }
+
+    /**
+     * Create the tables and populate them with initial Core data.
+     *
+     * @param string|null $basePath
+     *   Base path to the Codebase.
+     * @param string|null $definitionPath
+     *   Path to the resource definitions, relative to basePath.
+     * @param bool|null $includeTest
+     *   Create the Test account, application and user.
+     */
+    public function createTables(string $basePath = null, string $definitionPath = null, bool $includeTest = null)
     {
         echo "Creating and populating the tables with Core data...\n";
 
-        $include_test = '';
-        while ($include_test != 'y' && $include_test != 'n') {
-            $prompt = 'Include test users and accounts [y/N]: ';
-            $include_test = $this->readlineTerminal($prompt);
-            $include_test = empty($include_test) ? 'n' : $include_test;
+        try {
+            $basePath = $basePath === null ? $this->config->__get(['api', 'base_path']) : $basePath;
+            $definitionPath = $definitionPath === null ? $this->config->__get(['db', 'definition_path']) : $definitionPath;
+        } catch (ApiException $e) {
+            echo "Error: Create tables failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
         }
-        $path = $this->config->__get(['api', 'base_path']) . $this->config->__get(['db', 'definition_path']);
+
+        while (!is_bool($includeTest)) {
+            $prompt = 'Include test users and accounts [y/N]: ';
+            $includeTest = strtolower($this->readlineTerminal($prompt));
+            $includeTest = $includeTest === 'y' ? true : $includeTest;
+            $includeTest = $includeTest === 'n' || $includeTest === '' ? false : $includeTest;
+        }
+        $path = $basePath . $definitionPath;
         $yaml = file_get_contents($path);
         $definition = \Spyc::YAMLLoadString($yaml);
 
@@ -169,7 +380,7 @@ class Install extends Script
                 $sqlColumn = "`$column` ";
                 if (!isset($columnData['type'])) {
                     echo "CREATE TABLE `$table` failed!\n";
-                    echo "Type missing in the metadata.\n";
+                    echo "Error: Type missing in the metadata for table `$table`, please check the DB definition ($definitionPath).\n";
                     exit;
                 }
                 $sqlColumn .= ' ' . $columnData['type'];
@@ -181,23 +392,25 @@ class Install extends Script
                 $sqlColumns[] = $sqlColumn;
             }
             $sqlDrop = "DROP TABLE IF EXISTS `$table`";
-            $this->db->execute($sqlDrop);
+            if (!$this->db->execute($sqlDrop)) {
+                echo "Error: Failed to drop table `$table`, please check the logs.\n";
+            }
             $sqlCreate = "CREATE TABLE IF NOT EXISTS `$table` (" . implode(', ', $sqlColumns) . ');';
-            if (empty($this->db->execute($sqlCreate))) {
+            if (!($this->db->execute($sqlCreate))) {
                 // Stop if table create fails.
                 echo "$sqlCreate\n";
-                echo "CREATE TABLE `$table` fail!\n";
+                echo "Error: Failed to create the table `$table`, please check the logs.\n";
                 exit;
             }
 
             // Add data if required.
             if (isset($tableData['data'])) {
                 foreach ($tableData['data'] as $row) {
-                    if ($table == 'application' && $row['name'] == 'testing' && !$include_test) {
+                    if ($table == 'application' && $row['name'] == 'testing' && !$includeTest) {
                         // Do not create the testing application.
                         continue;
                     }
-                    if (($table == 'user' || $table == 'user_role') && !$include_test) {
+                    if (($table == 'user' || $table == 'user_role') && !$includeTest) {
                         // Do not create the tester user and associated roles.
                         continue;
                     }
@@ -209,26 +422,40 @@ class Install extends Script
                     }
                     $sqlRow = "INSERT INTO `$table` (" . implode(', ', $keys) . ')';
                     $sqlRow .= 'VALUES (' . implode(', ', $values) . ');';
-                    if (empty($this->db->execute($sqlRow))) {
+                    if (!($this->db->execute($sqlRow))) {
                         echo "$sqlRow\n";
-                        echo "INSERT into `$table` fail!\n";
+                        echo "Error: failed to insert a row into `$table`, please check the logs.\n";
                         exit;
                     }
                 }
             }
         }
+
         echo "Tables successfully initialised!\n";
     }
 
     /**
      * Add the Core resources to the DB.
      *
-     * @throws ApiException
+     * @param string|null $basePath
+     *   Base path to the Codebase.
+     * @param string|null $dirResources
+     *   Path to the resources directory, relative to basePath.
      */
-    protected function createResources()
+    public function createResources(string $basePath = null, string $dirResources = null)
     {
         echo "Adding core resources to the database...\n";
-        $dir = $this->config->__get(['api', 'base_path']) . $this->config->__get(['api', 'dir_resources']);
+
+        try {
+            $basePath = $basePath === null ? $this->config->__get(['api', 'base_path']) : $basePath;
+            $dirResources = $dirResources === null ? $this->config->__get(['api', 'dir_resources']) : $dirResources;
+        } catch (ApiException $e) {
+            echo "Error: Create resources failed, please check your settings.yml file.\n";
+            echo $e->getMessage() . "\n";
+            exit;
+        }
+
+        $dir = $basePath . $dirResources;
         $filenames = scandir($dir);
 
         foreach ($filenames as $filename) {
@@ -252,23 +479,30 @@ class Install extends Script
             $meta = '{' . implode(', ', $meta) . '}';
             $sqlRow = 'INSERT INTO resource (`appid`, `name`, `description`, `method`, `uri`, `meta`, `ttl`)';
             $sqlRow .= "VALUES ($appid, '$name', '$description', '$method', '$uri', '$meta', $ttl)";
-            if (empty($this->db->execute($sqlRow))) {
+            if (!($this->db->execute($sqlRow))) {
                 echo "$sqlRow\n";
-                echo "INSERT $name into `resource` fail!\n";
+                echo "Error: insert resource `$name` failed, please check your logs.\n";
                 exit;
             }
         }
+
         echo "Resources successfully added to the DB!\n";
     }
 
     /**
      * Create administrator user.
+     *
+     * @param string $username
+     *   Admin user username.
+     * @param string $password
+     *   Admin user password.
+     * @param string $email
+     *   Admin user email.
      */
-    protected function createAdminUser()
+    public function createAdminUser(string $username = '', string $password = '', string $email = '')
     {
         echo "Creating the admin user...\n";
 
-        $username = $password = $email = '';
         while ($username == '') {
             $prompt = 'Enter the admin users username: ';
             $username = $this->readlineTerminal($prompt);
@@ -311,7 +545,7 @@ class Install extends Script
             $user->setPassword($password);
             $userMapper->save($user);
         } catch (ApiException $e) {
-            echo "Error: an error occurred creating your user\n";
+            echo "Error: an error occurred creating your user, please check the logs.\n";
             echo $e->getMessage() . "\n";
             exit;
         }
@@ -323,13 +557,13 @@ class Install extends Script
             $user = $userMapper->findByUsername($username);
             $uid = $user->getUid();
             if (empty($uid)) {
-                echo "Error: Could not find the newly created user.\n";
+                echo "Error: Could not find the newly created user, please check the logs.\n";
                 exit;
             }
             $roleMapper = new Db\RoleMapper($this->db);
             $role = $roleMapper->findByName('Administrator');
             if (empty($uid)) {
-                echo "Error: Could not find the administrator role.\n";
+                echo "Error: Could not find the administrator role, please check the logs.\n";
                 exit;
             }
             $rid = $role->getRid();
@@ -343,8 +577,8 @@ class Install extends Script
             $userRoleMapper = new Db\UserRoleMapper($this->db);
             $userRoleMapper->save($userRole);
         } catch (ApiException $e) {
-            echo "Error: An error occurred creating your Administrator role\n";
-            echo $e->getMessage();
+            echo "Error: An error occurred creating your Administrator role, please check the logs.\n";
+            echo $e->getMessage() . "\n";
             exit;
         }
 
