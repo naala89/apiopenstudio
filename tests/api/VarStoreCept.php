045e694d4f1e32058b509fc79def1606a110707d
John <3242482-john89@users.noreply.gitlab.com>
2021-11-21 09:11:03 +1100

    #127 VarStoreRead setting expected_type and VarStoreCept fixes

--- tests/api/VarStoreCept.php
+++ tests/api/VarStoreCept.php
@@ -2,6 +2,7 @@
 
 $I = new ApiTester($scenario);
 $uri = $I->getCoreBaseUri() . '/var_store';
+$varStores = [];
 
 // Test role access to create var_store.
 
@@ -36,7 +37,10 @@ $I->sendPost($uri, ['appid' => 2, 'key' => 'varkey1', 'val' => 'varval1']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.key');
+$I->seeResponseJsonMatchesJsonPath('$.vid');
 $I->seeResponseJsonMatchesJsonPath('$.val');
+$response = json_decode($I->getResponse(), true);
+$varStores[$response['key']] = $response['vid'];
 
 $I->wantTo('Test a developer cannot create a var for an application they are not assigned to');
 $I->sendPost($uri, ['appid' => 1, 'key' => 'varkey2', 'val' => 'varval2']);
@@ -56,7 +60,10 @@ $I->sendPost($uri, ['appid' => 2, 'key' => 'varkey2', 'val' => 'varval2']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.key');
+$I->seeResponseJsonMatchesJsonPath('$.vid');
 $I->seeResponseJsonMatchesJsonPath('$.val');
+$response = json_decode($I->getResponse(), true);
+$varStores[$response['key']] = $response['vid'];
 
 $I->wantTo('Test an application manager cannot create a var for an application they are not assigned to');
 $I->sendPost($uri, ['appid' => 1, 'key' => 'varkey3', 'val' => 'varval3']);
@@ -76,7 +83,10 @@ $I->sendPost($uri, ['appid' => 2, 'key' => 'varkey3', 'val' => 'varval3']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.key');
+$I->seeResponseJsonMatchesJsonPath('$.vid');
 $I->seeResponseJsonMatchesJsonPath('$.val');
+$response = json_decode($I->getResponse(), true);
+$varStores[$response['key']] = $response['vid'];
 
 $I->wantTo('Test an account manager cannot create a var for an application in an account they are not assigned to');
 $I->sendPost($uri, ['appid' => 1, 'key' => 'varkey4', 'val' => 'varval4']);
@@ -96,12 +106,18 @@ $I->sendPost($uri, ['appid' => 2, 'key' => 'varkey4', 'val' => 'varval4']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.key');
+$I->seeResponseJsonMatchesJsonPath('$.vid');
 $I->seeResponseJsonMatchesJsonPath('$.val');
+$response = json_decode($I->getResponse(), true);
+$varStores[$response['key']] = $response['vid'];
 $I->sendPost($uri, ['appid' => 1, 'key' => 'varkey5', 'val' => 'varval5']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.key');
+$I->seeResponseJsonMatchesJsonPath('$.vid');
 $I->seeResponseJsonMatchesJsonPath('$.val');
+$response = json_decode($I->getResponse(), true);
+$varStores[$response['key']] = $response['vid'];
 
 // Test role access to read var_store.
 
@@ -117,31 +133,33 @@ $I->performLogin(getenv('TESTER_CONSUMER_NAME'), getenv('TESTER_CONSUMER_PASS'))
 
 // phpcs:ignore
 $I->wantTo('Test a consumer cannot read a var within an application they are assigned to with validate_access set to true, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => true]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => true]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([]);
 
 // phpcs:ignore
 $I->wantTo('Test a consumer can read a var within an application they are assigned to with validate_access set to false, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test a consumer cannot read a var within an application they are NOT assigned to with validate_access as default true, by vid');
-$I->sendGet($uri, ['vid' => 9]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5']]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([]);
 
 // phpcs:ignore
 $I->wantTo('Test a consumer can read a var within an application they are NOT assigned to with validate_access set to false, by vid ');
-$I->sendGet($uri, ['vid' => 9, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
@@ -149,32 +167,35 @@ $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'
 
 // phpcs:ignore
 $I->wantTo('Test a developer can read a var within an application they are assigned to with validate_access set to true, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => true]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test a developer can read a var within an application they are assigned to with validate_access set to false, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test a developer cannot read a var within an application they are NOT assigned to with validate_access as default true, by vid');
-$I->sendGet($uri, ['vid' => 9]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5']]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([]);
 
 // phpcs:ignore
 $I->wantTo('Test a developer can read a var within an application they are NOT assigned to with validate_access set to false, by vid ');
-$I->sendGet($uri, ['vid' => 9, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
@@ -182,32 +203,35 @@ $I->performLogin(getenv('TESTER_APPLICATION_MANAGER_NAME'), getenv('TESTER_APPLI
 
 // phpcs:ignore
 $I->wantTo('Test an application manager can read a var within an application they are assigned to with validate_access set to true, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => true]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an application manager can read a var within an application they are assigned to with validate_access set to false, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an application manager cannot read a var within an application they are NOT assigned to with validate_access as default true, by vid');
-$I->sendGet($uri, ['vid' => 9]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5']]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([]);
 
 // phpcs:ignore
 $I->wantTo('Test an application manager can read a var within an application they are NOT assigned to with validate_access set to false, by vid ');
-$I->sendGet($uri, ['vid' => 9, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
@@ -215,32 +239,35 @@ $I->performLogin(getenv('TESTER_ACCOUNT_MANAGER_NAME'), getenv('TESTER_ACCOUNT_M
 
 // phpcs:ignore
 $I->wantTo('Test an account manager can read a var within an application they are assigned to with validate_access set to true, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => true]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an account manager can read a var within an application they are assigned to with validate_access set to false, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an account manager cannot read a var within an application they are NOT assigned to with validate_access as default true, by vid');
-$I->sendGet($uri, ['vid' => 9]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5']]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([]);
 
 // phpcs:ignore
 $I->wantTo('Test an account manager can read a var within an application they are NOT assigned to with validate_access set to false, by vid ');
-$I->sendGet($uri, ['vid' => 9, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
@@ -248,45 +275,48 @@ $I->performLogin(getenv('TESTER_ADMINISTRATOR_NAME'), getenv('TESTER_ADMINISTRAT
 
 // phpcs:ignore
 $I->wantTo('Test an administrator can read a var within an application they are assigned to with validate_access set to true, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => true]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => true]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an administrator can read a var within an application they are assigned to with validate_access set to false, by vid');
-$I->sendGet($uri, ['vid' => 5, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey1'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an administrator cannot read a var within an application they are NOT assigned to with validate_access as default true, by vid');
-$I->sendGet($uri, ['vid' => 9]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5']]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // phpcs:ignore
 $I->wantTo('Test an administrator can read a var within an application they are NOT assigned to with validate_access set to false, by vid ');
-$I->sendGet($uri, ['vid' => 9, 'validate_access' => false]);
+$I->sendGet($uri, ['vid' => $varStores['varkey5'], 'validate_access' => false]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$..vid');
 $I->seeResponseJsonMatchesJsonPath('$..key');
 $I->seeResponseJsonMatchesJsonPath('$..val');
 
 // Clean up
+
 $uri = $I->getCoreBaseUri() . '/var_store';
 
 $I->performLogin(getenv('TESTER_ADMINISTRATOR_NAME'), getenv('TESTER_ADMINISTRATOR_PASS'));
-$I->sendDelete($uri, ['vid' => 9]);
+foreach ($varStores as $vid) {
+    $I->sendDelete($uri . '/' . $vid);
+}
 
 $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
 $I->tearDownTestFromYaml($yamlFilename);
-$I->sendDelete($uri, ['vid' => 5]);
-$I->sendDelete($uri, ['vid' => 6]);
-$I->sendDelete($uri, ['vid' => 7]);
-$I->sendDelete($uri, ['vid' => 8]);
