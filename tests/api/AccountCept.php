b0180eb8946a6e7f07641185f13710371b2b545b
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2021-12-04 12:00:31 +1100

    #48 new AccountCept test

--- tests/api/AccountCept.php
+++ tests/api/AccountCept.php
@@ -2,36 +2,127 @@
 
 $I = new ApiTester($scenario);
 
-$validCreateEditDeleteRoles = [
-    ['username' => getenv('TESTER_ADMINISTRATOR_NAME'), 'password' => getenv('TESTER_ADMINISTRATOR_NAME')],
+$validCreateEditDeleteUsers = [
+    ['username' => getenv('TESTER_ADMINISTRATOR_NAME'), 'password' => getenv('TESTER_ADMINISTRATOR_PASS')],
 ];
-$invalidCreateEditDeleteRoles = [
+$invalidCreateEditDeleteUsers = [
     ['username' => getenv('TESTER_ACCOUNT_MANAGER_NAME'), 'password' => getenv('TESTER_ACCOUNT_MANAGER_PASS')],
     ['username' => getenv('TESTER_APPLICATION_MANAGER_NAME'), 'password' => getenv('TESTER_APPLICATION_MANAGER_PASS')],
     ['username' => getenv('TESTER_DEVELOPER_NAME'), 'password' => getenv('TESTER_DEVELOPER_PASS')],
     ['username' => getenv('TESTER_CONSUMER_NAME'), 'password' => getenv('TESTER_CONSUMER_PASS')],
 ];
-$validReadRoles = [
-    ['username' => getenv('TESTER_ADMINISTRATOR_NAME'), 'password' => getenv('TESTER_ADMINISTRATOR_NAME')],
-    ['username' => getenv('TESTER_ACCOUNT_MANAGER_NAME'), 'password' => getenv('TESTER_ACCOUNT_MANAGER_PASS')],
-    ['username' => getenv('TESTER_APPLICATION_MANAGER_NAME'), 'password' => getenv('TESTER_APPLICATION_MANAGER_PASS')],
-    ['username' => getenv('TESTER_DEVELOPER_NAME'), 'password' => getenv('TESTER_DEVELOPER_PASS')],
-    ['username' => getenv('TESTER_CONSUMER_NAME'), 'password' => getenv('TESTER_CONSUMER_PASS')],
+$validReadUsers = [
+    [
+        'username' => getenv('TESTER_ADMINISTRATOR_NAME'),
+        'password' => getenv('TESTER_ADMINISTRATOR_PASS'),
+        'accounts' => [
+            [
+                'accid' => 1,
+                'name' => 'apiopenstudio',
+            ], [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ],
+    ],
+    [
+        'username' => getenv('TESTER_ACCOUNT_MANAGER_NAME'),
+        'password' => getenv('TESTER_ACCOUNT_MANAGER_PASS'),
+        'accounts' => [
+            [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ],
+    ], [
+        'username' => getenv('TESTER_APPLICATION_MANAGER_NAME'),
+        'password' => getenv('TESTER_APPLICATION_MANAGER_PASS'),
+        'accounts' => [
+            [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ],
+    ], [
+        'username' => getenv('TESTER_DEVELOPER_NAME'),
+        'password' => getenv('TESTER_DEVELOPER_PASS'),
+        'accounts' => [
+            [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ],
+    ], [
+        'username' => getenv('TESTER_CONSUMER_NAME'),
+        'password' => getenv('TESTER_CONSUMER_PASS'),
+        'accounts' => [
+            'accid' => 2,
+            'name' => 'testing_acc',
+        ],
+    ],
 ];
 
 // Test account generation for each role
-$uri = $I->getMyBaseUri() . '/account';
-foreach ($validCreateEditDeleteRoles as $validCreateEditDeleteRole) {
-    $I->performLogin($validCreateEditDeleteRole['username'], $validCreateEditDeleteRole['password']);
-    $I->sendPost($uri, ['new_account1']);
+$uri = $I->getCoreBaseUri() . '/account';
+$accid = 0;
+foreach ($validCreateEditDeleteUsers as $user) {
+    $I->performLogin($user['username'], $user['password']);
+    $I->sendPost($uri, ['name' => 'new_account1']);
     $I->seeResponseCodeIs(200);
     $I->seeResponseIsJson();
     $I->seeResponseJsonMatchesJsonPath('$.accid');
-    $I->seeResponseJsonMatchesJsonPath('$.name');
+    $response = json_decode($I->getResponse(), true);
+    $accid = $response['accid'];
+    $I->seeResponseContainsJson([
+        'accid' => $accid,
+        'name' => 'new_account1'
+    ]);
+
+    $I->sendPut("$uri/$accid/edited_name");
+    $I->seeResponseCodeIs(200);
+    $I->seeResponseIsJson();
+    $I->seeResponseContainsJson([
+        'accid' => $accid,
+        'name' => 'edited_name'
+    ]);
+
+    $I->sendDelete("$uri/$accid");
+    $I->seeResponseCodeIs(200);
+    $I->seeResponseIsJson();
+    $I->seeResponseContains('true');
 }
-foreach ($invalidCreateEditDeleteRoles as $invalidCreateEditDeleteRole) {
-    $I->performLogin($invalidCreateEditDeleteRole['username'], $invalidCreateEditDeleteRole['password']);
-    $I->sendPost($uri, ['new_account1']);
+
+$I->performLogin(getenv('TESTER_ADMINISTRATOR_NAME'), getenv('TESTER_ADMINISTRATOR_PASS'));
+$I->sendPost($uri, ['name' => 'new_account1']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$.accid');
+$response = json_decode($I->getResponse(), true);
+$accid = $response['accid'];
+$I->seeResponseContainsJson([
+    'accid' => $accid,
+    'name' => 'new_account1'
+]);
+
+foreach ($invalidCreateEditDeleteUsers as $user) {
+    $I->performLogin($user['username'], $user['password']);
+    $I->sendPost($uri, ['name' => 'new_account2']);
+    $I->seeResponseCodeIs(401);
+    $I->seeResponseIsJson();
+    $I->seeResponseJsonMatchesJsonPath('$.error');
+    $I->seeResponseJsonMatchesJsonPath('$.error.code');
+    $I->seeResponseJsonMatchesJsonPath('$.error.id');
+    $I->seeResponseJsonMatchesJsonPath('$.error.message');
+
+    $I->sendPut("$uri/$accid/edited_name");
+    $I->seeResponseCodeIs(401);
+    $I->seeResponseIsJson();
+    $I->seeResponseJsonMatchesJsonPath('$.error');
+    $I->seeResponseJsonMatchesJsonPath('$.error.code');
+    $I->seeResponseJsonMatchesJsonPath('$.error.id');
+    $I->seeResponseJsonMatchesJsonPath('$.error.message');
+
+    $I->sendDelete("$uri/$accid");
     $I->seeResponseCodeIs(401);
     $I->seeResponseIsJson();
     $I->seeResponseJsonMatchesJsonPath('$.error');
@@ -39,3 +130,76 @@ foreach ($invalidCreateEditDeleteRoles as $invalidCreateEditDeleteRole) {
     $I->seeResponseJsonMatchesJsonPath('$.error.id');
     $I->seeResponseJsonMatchesJsonPath('$.error.message');
 }
+
+// Test all account read for all users.
+foreach ($validReadUsers as $user) {
+    $I->performLogin($user['username'], $user['password']);
+    $I->sendGet("$uri");
+    $I->seeResponseCodeIs(200);
+    $I->seeResponseIsJson();
+    $I->seeResponseContainsJson($user['accounts']);
+}
+
+// Test individual account read for a user
+foreach ($validReadUsers as $user) {
+    if ($user['username'] != getenv('TESTER_ADMINISTRATOR_NAME')) {
+        $I->performLogin($user['username'], $user['password']);
+        $I->sendGet("$uri/1");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([]);
+
+        $I->sendGet("$uri/2");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ]);
+
+        $I->sendGet("$uri/$accid");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([]);
+    } else {
+        $I->performLogin($user['username'], $user['password']);
+        $I->sendGet("$uri/1");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            [
+                'accid' => 1,
+                'name' => 'apiopenstudio',
+            ],
+        ]);
+
+        $I->sendGet("$uri/2");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            [
+                'accid' => 2,
+                'name' => 'testing_acc',
+            ],
+        ]);
+
+        $I->sendGet("$uri/$accid");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            [
+                'accid' => $accid,
+                'name' => 'new_account1',
+            ],
+        ]);
+    }
+}
+
+// Clean up
+$I->performLogin(getenv('TESTER_ADMINISTRATOR_NAME'), getenv('TESTER_ADMINISTRATOR_PASS'));
+$I->sendDelete("$uri/$accid");
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContains('true');
