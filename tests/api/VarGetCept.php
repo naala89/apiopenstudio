350b8474456e3cff9b2dc8f3e70d99c25618ca38
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-04-02 13:02:34 +1100

    Refs #158 - fix VarGetCept tests.

--- tests/api/VarGetCept.php
+++ tests/api/VarGetCept.php
@@ -15,82 +15,107 @@ $I->wantTo('populate a VarGet with text and see the result.');
 $I->sendGet($uri, ['value' => 'text']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('text');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 'text',
+]);
 
 $I->wantTo('populate a VarGet with true and see the result.');
 $I->sendGet($uri, ['value' => 'true']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('true');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 'true',
+]);
 
 $I->wantTo('populate a VarGet with 1.6 and see the result.');
 $I->sendGet($uri, ['value' => '1.6']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('1.6');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 1.6,
+]);
 
 $I->wantTo('populate a VarGet with 1.6 and see the result.');
 $I->sendGet($uri, ['value' => 1.6]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('1.6');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 1.6,
+]);
 
 $I->wantTo('populate a VarGet with 1 and see the result.');
 $I->sendGet($uri, ['value' => 1.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('1');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 1,
+]);
 
 $I->wantTo('populate a VarGet with 1.0 and see the result.');
 $I->sendGet($uri, ['value' => 1]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('1');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 1,
+]);
 
 $I->wantTo('populate a VarGet with 0 and see the result.');
 $I->sendGet($uri, ['value' => 0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('0');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 0,
+]);
 
 $I->wantTo('populate a VarGet with 0.0 and see the result.');
 $I->sendGet($uri, ['value' => 0.0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('0');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => 0,
+]);
 
 $I->wantTo('populate a VarGet with wrong varname and nullable true and see the result.');
 $I->sendGet($uri, ['values' => 'test', 'nullable' => true]);
 $I->seeResponseCodeIs(200);
-$I->seeResponseEquals('');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => '',
+]);
 
 $I->wantTo('populate a VarGet with wrong varname and nullable false and see the result.');
 $I->sendGet($uri, ['values' => 'test', 'nullable' => false]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(
-    [
-        'error' => [
-            'code' => 6,
-            'message' => "GET variable (value) does not exist or is empty.",
-            'id' => 'test var_get process',
-        ],
-    ]
-);
+$I->seeResponseContainsJson([
+    'result' => 'error',
+    'data' => [
+        'code' => 6,
+        'message' => "GET variable (value) does not exist or is empty.",
+        'id' => 'test var_get process',
+    ],
+]);
 
 $I->wantTo('populate a VarGet with wrong varname and nullable not set and see the result.');
 $I->sendGet($uri, ['values' => 'test']);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(
-    [
-        'error' => [
-            'code' => 6,
-            'message' => "GET variable (value) does not exist or is empty.",
-            'id' => 'test var_get process',
-        ],
-    ]
-);
+$I->seeResponseContainsJson([
+    'result' => 'error',
+    'data' => [
+        'code' => 6,
+        'message' => "GET variable (value) does not exist or is empty.",
+        'id' => 'test var_get process',
+    ],
+]);
 
 $I->deleteHeader('Authorization');
 $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
