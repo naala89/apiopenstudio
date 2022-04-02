9bfe5e8c40ccfe1b7860ef0eab51b7a68b8f4b81
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-04-02 13:02:07 +1100

    Refs #158 - fix VarBoolCept tests.

--- tests/api/VarBoolCept.php
+++ tests/api/VarBoolCept.php
@@ -14,63 +14,99 @@ $I->wantTo('populate a VarBool with 1 and see the result.');
 $I->sendGet($uri, ['value' => '1']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('true');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => true,
+]);
 
 $I->wantTo('populate a VarBool with 0 and see the result.');
 $I->sendGet($uri, ['value' => '0']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('false');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => false,
+]);
 
 $I->wantTo('populate a VarBool with 1 and see the result.');
 $I->sendGet($uri, ['value' => 1]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('true');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => true,
+]);
 
 $I->wantTo('populate a VarBool with 0 and see the result.');
 $I->sendGet($uri, ['value' => 0]);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('false');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => false,
+]);
 
 $I->wantTo('populate a VarBool with yes and see the result.');
 $I->sendGet($uri, ['value' => 'yes']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('true');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => true,
+]);
 
 $I->wantTo('populate a VarBool with no and see the result.');
 $I->sendGet($uri, ['value' => 'no']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('false');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => false,
+]);
 
 $I->wantTo('populate a VarBool with true and see the result.');
 $I->sendGet($uri, ['value' => 'true']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('true');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => true,
+]);
 
 $I->wantTo('populate a VarBool with 0 and see the result.');
 $I->sendGet($uri, ['value' => 'false']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('false');
+$I->seeResponseContainsJson([
+    'result' => 'ok',
+    'data' => false,
+]);
 
 $I->wantTo('populate a VarBool with 6 and see the result.');
 $I->sendGet($uri, ['value' => 6]);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
-$I->seeResponseJsonMatchesJsonPath('error.message');
-$I->seeResponseJsonMatchesJsonPath('error.id');
+$I->seeResponseContainsJson([
+    'result' => 'error',
+    'data' => [
+        'code' => 6,
+        'message' => 'Failed to convert text to boolean.',
+        'id' => 'test var_bool value',
+    ],
+]);
 
 $I->wantTo('populate a VarBool with fales and see the result.');
 $I->sendGet($uri, ['value' => 'fales']);
 $I->seeResponseCodeIs(400);
 $I->seeResponseIsJson();
-$I->seeResponseJsonMatchesJsonPath('error.message');
-$I->seeResponseJsonMatchesJsonPath('error.id');
+$I->seeResponseContainsJson([
+    'result' => 'error',
+    'data' => [
+        'code' => 6,
+        'message' => 'Failed to convert text to boolean.',
+        'id' => 'test var_bool value',
+    ],
+]);
 
 $I->deleteHeader('Authorization');
 $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
