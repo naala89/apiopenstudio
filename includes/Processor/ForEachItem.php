4da3d033fc3c2d90616b59db737de4dbe98de04c
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-04-13 14:30:40 +1000

    Refs #156 - PHPDoc for ForEachItem.

--- includes/Processor/ForEachItem.php
+++ includes/Processor/ForEachItem.php
@@ -8,8 +8,7 @@
  *   <for_each_item.id>.key
  *   <for_each_item.id>.val
  *
- * @package    ApiOpenStudio
- * @subpackage Processor
+ * @package    ApiOpenStudio\Processor
  * @author     john89 (https://gitlab.com/john89)
  * @copyright  2020-2030 Naala Pty Ltd
  * @license    This Source Code Form is subject to the terms of the ApiOpenStudio Public License.
@@ -83,7 +82,7 @@ class ForEachItem extends ProcessorEntity
     ];
 
     /**
-     * @var \DeepCopy\DeepCopy
+     * @var DeepCopy
      *   Enable deepy copy of objects.
      */
     protected DeepCopy $deepCopy;
