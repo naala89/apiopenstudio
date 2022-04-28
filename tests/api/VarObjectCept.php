504f9406a918316f67df9540ec2ecf0284096a99
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-04-28 15:54:36 +1000

    Refs #76 - Test for valid array of complex object literals.

--- tests/api/VarObjectCept.php
+++ tests/api/VarObjectCept.php
@@ -99,25 +99,33 @@ $I->seeResponseContainsJson([
 $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
 $I->tearDownTestFromYaml($yaml);
 
-//$I->wantTo('create an Object processor of fields with literals and see result');
-//$yaml = 'objectArrayTest.yaml';
-//$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
-//$I->createResourceFromYaml($yaml);
-//$I->deleteHeader('Authorization');
-//$uri = $I->getMyBaseUri() . '/object/array';
-//$I->sendGet($uri, ['token' => $I->getMyStoredToken()]);
-//$I->seeResponseCodeIs(200);
-//$I->seeResponseIsJson();
-//$I->seeResponseContainsJson([
-//    "key1" => "field1",
-//    "key2" => "field2",
-//    "key3" => "field3",
-//    "key4" => "field4"
-//]);
-//$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
-//$I->tearDownTestFromYaml($yaml);
-//$I->deleteHeader('Authorization');
-//
+$I->wantTo('create an Object processor of fields with literals and see result');
+$yaml = 'varObjectObjectsComplexTest.yaml';
+$uri = '/object/array';
+$I->createResourceFromYaml($yaml);
+$I->performLogin(getenv('TESTER_CONSUMER_NAME'), getenv('TESTER_CONSUMER_PASS'));
+$I->sendGet($I->getMyBaseUri() . $uri);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+    'key1_1' => [
+        'key1_2_1' => 'val1_1',
+    ],
+    'key2_1' => [
+        'key2_2_1' => 'val2_2_1',
+        'key2_2_2' => [
+            'key2_3_1' => 'val2_3_1',
+        ],
+    ],
+    'key3_1' => 'val3_1',
+    'key4_1' => [
+        'val4_2_1',
+        'val4_2_2',
+    ]
+]);
+$I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
+$I->tearDownTestFromYaml($yaml);
+
 //$I->wantTo('create an Object processor of complex arrays with literals and see result');
 //$yaml = 'objectArrayComplexTest.yaml';
 //$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
