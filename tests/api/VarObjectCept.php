d67a0a3866d82db8b1caebaeb6820a44caab4ab0
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-04-28 15:56:35 +1000

    Refs #76 - Tidy up tests file.

--- tests/api/VarObjectCept.php
+++ tests/api/VarObjectCept.php
@@ -125,24 +125,3 @@ $I->seeResponseContainsJson([
 ]);
 $I->performLogin(getenv('TESTER_DEVELOPER_NAME'), getenv('TESTER_DEVELOPER_PASS'));
 $I->tearDownTestFromYaml($yaml);
-
-//$I->wantTo('create an Object processor of complex arrays with literals and see result');
-//$yaml = 'objectArrayComplexTest.yaml';
-//$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
-//$I->createResourceFromYaml($yaml);
-//$I->deleteHeader('Authorization');
-//$uri = $I->getMyBaseUri() . '/object/array/complex';
-//$I->sendGet($uri, ['token' => $I->getMyStoredToken()]);
-//$I->seeResponseCodeIs(200);
-//$I->seeResponseIsJson();
-//$I->seeResponseContainsJson([
-//    "key1" => [
-//        "key1_1" => "val1_1",
-//    ],
-//    "key2" => "field2",
-//    "key3" => "field3",
-//    "key4" => "field4"
-//]);
-//$I->haveHttpHeader('Authorization', 'Bearer ' . $I->getMyStoredToken());
-//$I->tearDownTestFromYaml($yaml);
-//$I->deleteHeader('Authorization');
