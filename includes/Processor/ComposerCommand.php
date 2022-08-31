57c16ae7d3df940692ab86b2aab8a54bef0c844c
laughing_man77 <8545282-laughing_man77@users.noreply.gitlab.com>
2022-08-31 10:17:13 +1000

    Refs #201 - Added composer repository add, unset, list functionality to ComposerCommand processor.

--- includes/Processor/ComposerCommand.php
+++ includes/Processor/ComposerCommand.php
@@ -20,7 +20,7 @@ use ApiOpenStudio\Core\DataContainer;
 use ApiOpenStudio\Core\ProcessorEntity;
 use Composer\Console\Application;
 use Exception;
-use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
@@ -47,12 +47,30 @@ class ComposerCommand extends ProcessorEntity
                 'literalAllowed' => true,
                 'limitProcessors' => [],
                 'limitTypes' => ['text'],
-                'limitValues' => ['install', 'require', 'remove'],
+                'limitValues' => ['repository_list', 'repository_set', 'repository_unset', 'require', 'remove'],
                 'default' => '',
             ],
             'package' => [
                 'description' => 'Package to install or remove.',
-                'cardinality' => [1, '*'],
+                'cardinality' => [0, 1],
+                'literalAllowed' => true,
+                'limitProcessors' => [],
+                'limitTypes' => ['text'],
+                'limitValues' => [],
+                'default' => '',
+            ],
+            'repository_url' => [
+                'description' => 'Repository url to add to composer.',
+                'cardinality' => [0, 1],
+                'literalAllowed' => true,
+                'limitProcessors' => [],
+                'limitTypes' => ['text'],
+                'limitValues' => [],
+                'default' => '',
+            ],
+            'repository_key' => [
+                'description' => 'Repository name to add to composer.',
+                'cardinality' => [0, 1],
                 'literalAllowed' => true,
                 'limitProcessors' => [],
                 'limitTypes' => ['text'],
@@ -63,24 +81,82 @@ class ComposerCommand extends ProcessorEntity
     ];
 
     /**
-     * {@inheritDoc}
-     *
-     * @return DataContainer Result of the processor.
-     *
-     * @throws ApiException Exception if invalid result.
+     * @return DataContainer
+     * @throws ApiException
      */
     public function process(): DataContainer
     {
         parent::process();
         $command = $this->val('command', true);
-        $packages = $this->val('package', true);
-        $packages = is_array($packages) ? $packages : [$packages];
-        $input = new ArrayInput([
-            'command' => $command,
-            'packages' => $packages,
-        ]);
-        $input->setInteractive(false);
+        $package = $this->val('package', true);
+        $key = $this->val('repository_key', true);
+        $url = $this->val('repository_url', true);
 
+        switch ($command) {
+            case 'require':
+                if (empty($package)) {
+                    throw new ApiException('No package names received', 6, $this->id, 400);
+                }
+                $input = new StringInput("$command --with-all-dependencies $package");
+                return $this->runCommand($input);
+            case 'remove':
+                if (empty($package)) {
+                    throw new ApiException('No package names received', 6, $this->id, 400);
+                }
+                $input = new StringInput("$command $package");
+                return $this->runCommand($input);
+            case 'repository_set':
+                if (empty($key)) {
+                    throw new ApiException('No repository key received', 6, $this->id, 400);
+                }
+                if (empty($url)) {
+                    throw new ApiException('No repository URL received', 6, $this->id, 400);
+                }
+                $input = new StringInput("config repositories.$key vcs $url");
+                return $this->runCommand($input);
+            case 'repository_unset':
+                if (empty($key)) {
+                    throw new ApiException('No repository key received', 6, $this->id, 400);
+                }
+                $input = new StringInput("config --unset repositories.$key");
+                return $this->runCommand($input);
+            case 'repository_list':
+                $input = new StringInput("config --list");
+                $config = $this->runCommand($input);
+                $config = explode("\n", $config->getData());
+                $result = [];
+                foreach ($config as $line) {
+                    $line = str_replace('[', '', $line);
+                    $line = str_replace(']', '', $line);
+                    $items = explode(' ', $line);
+                    if (
+                        sizeof($items) == 2
+                        && strpos($items[0], 'repositories.') === 0
+                        && !in_array($items[1], ['vcs', 'composer'])
+                    ) {
+                        $items[0] = str_replace('repositories.', '', $items[0]);
+                        $items[0] = str_replace('.url', '', $items[0]);
+                        $result[$items[0]] = $items[1];
+                    }
+                }
+                return new DataContainer($result);
+            default:
+                throw new ApiException('Unhandled composer command', 6, $this->id, 400);
+        }
+    }
+
+    /**
+     * Run a composer command and return the cli response.
+     *
+     * @param $input
+     *
+     * @return DataContainer
+     *
+     * @throws ApiException
+     */
+    protected function runCommand($input)
+    {
+        $input->setInteractive(false);
         try {
             $result = $this->composerCommand($input);
         } catch (ApiException $e) {
@@ -93,12 +169,12 @@ class ComposerCommand extends ProcessorEntity
     /**
      * Run a composer command.
      *
-     * @param ArrayInput $input
+     * @param $input
      * @return string
      *
      * @throws ApiException
      */
-    protected function composerCommand(ArrayInput $input): string
+    protected function composerCommand($input): string
     {
         $settings = new Config();
         $cwd = getcwd();
